f032def46c8d6a37b365df92ea9329fed43f9b50
Yan-Fa Li <yanfali@gmail.com>
2013-06-18 15:38:34 -0700

    Add a basic visualization for cluster availability
    - trying out Raphael
    - round out corners a bit
    - idea is concentric circles representing different elements in the
      cluster with inner arcs representing failing/unreachable nodes
    
      A fully interactive version would have to support hover and
      click through to another visualization for the specific category

--- dashboard/app/scripts/main.js
+++ dashboard/app/scripts/main.js
@@ -29,11 +29,12 @@ require.config({
         bootstrap: 'vendor/bootstrap',
         gauge: 'vendor/gauge',
         bean: '../bower_components/bean/bean',
-        flotr2: 'vendor/flotr2.amd'
+        flotr2: 'vendor/flotr2.amd',
+        raphael: '../bower_components/raphael/raphael'
     }
 });
 
-require(['jquery', 'backbone', 'gauge', 'flotr2'], function($, Backbone, Gauge, Flotr) {
+require(['jquery', 'backbone', 'gauge', 'flotr2', 'raphael'], function($, Backbone, Gauge, Flotr) {
     Backbone.history.start();
     var opts = {
         lines: 10,
@@ -73,4 +74,125 @@ require(['jquery', 'backbone', 'gauge', 'flotr2'], function($, Backbone, Gauge, 
             minorVerticalLines: true
         }
     });
+    var $el = $('.raphael-one');
+    var ra = window.Raphael($el[0], 600, 600);
+    ra.customAttributes.arc = function(value, total, R, mycolor) {
+        var xorigin = 300;
+        var yorigin = 300;
+        var alpha = 360 / total * value,
+            a = (90 - alpha) * Math.PI / 180,
+            x = xorigin + R * Math.cos(a),
+            y = yorigin - R * Math.sin(a),
+            color = 'hsb('.concat(Math.round(R) / 200, ',', value / total, ', 0.75)'),
+            path;
+        if (mycolor !== undefined) {
+            color = mycolor;
+        }
+        if (total === value) {
+            path = [
+                ['M', xorigin, yorigin - R],
+                ['A', R, R, 0, 1, 1, 299.99, 300 - R]
+            ];
+        } else {
+            path = [
+                ['M', xorigin, yorigin - R],
+                ['A', R, R, 0, +(alpha > 180), 1, x, y]
+            ];
+        }
+        return {
+            path: path,
+            stroke: color
+        };
+    };
+    var $itred = '#f05c56',
+        $itdarkred = '#9c4850',
+        $itgray = '#5e6a71',
+        $itdarkgray = '#37424a',
+        $itlightgray = '#e6e8e8',
+        $itaqua = '#55aeba',
+        $itlightaqua = '#80d2dc';
+
+    (function() {
+        var radius = 200;
+        var param1 = {
+            'stroke-width': 50
+        };
+        ra.path().attr(param1).attr({
+            arc: [100, 100, radius, $itdarkred]
+        });
+        var param2 = {
+            'stroke-width': 30
+        };
+        ra.path().attr(param2).attr({
+            arc: [10, 100, radius]
+        });
+        param2 = {
+            'stroke-width': 20
+        };
+        ra.path().attr(param2).attr({
+            arc: [5, 100, radius, $itred]
+        });
+        ra.text(300, 100, 'OSDs').attr({
+            'font-family': 'ApexSansMedium',
+            'font-size': '20px'
+        });
+    }());
+
+    (function() {
+        var radius = 130;
+        var param1 = {
+            'stroke-width': 50
+        };
+        ra.path().attr(param1).attr({
+            arc: [100, 100, radius, $itaqua]
+        });
+        var param2 = {
+            'stroke-width': 30
+        };
+        ra.path().attr(param2).attr({
+            arc: [5, 100, radius]
+        });
+        param2 = {
+            'stroke-width': 20
+        };
+        ra.path().attr(param2).attr({
+            arc: [2, 100, radius, $itlightaqua]
+        });
+        ra.text(300, 425, 'Hosts').attr({
+            'font-family': 'ApexSansMedium',
+            'font-size': '20px',
+        });
+    }());
+
+    (function() {
+        var radius = 60;
+        var param1 = {
+            'stroke-width': 50
+        };
+        ra.path().attr(param1).attr({
+            arc: [100, 100, radius, $itgray]
+        });
+        var param2 = {
+            'stroke-width': 30
+        };
+        ra.path().attr(param2).attr({
+            arc: [10, 100, radius, $itlightgray]
+        });
+        param2 = {
+            'stroke-width': 20
+        };
+        ra.path().attr(param2).attr({
+            arc: [3, 100, radius, $itdarkgray]
+        });
+        ra.text(300, 294, 'Racks').attr({
+            'font-family': 'ApexSansMedium',
+            'font-size': '20px'
+        });
+    }());
+
+    ra.text(300, 20, 'Cluster Availability').attr({
+        'font-family': 'ApexSansMedium',
+        'font-size': '40px'
+    });
+
 });
