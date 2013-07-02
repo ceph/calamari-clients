5f5f5b0ec692aac2da4fba3c7fe1e450985c6bd5
Yan-Fa Li <yanfali@gmail.com>
2013-07-01 23:51:35 -0700

    Use a simple animation to switch OSD attributes
    There are some situations when switching animations causes visible
    jank. I'm not entirely sure what the cause it. It could be jquery
    manipulating the class list.

--- dashboard/app/scripts/views/application-view.js
+++ dashboard/app/scripts/views/application-view.js
@@ -9,10 +9,22 @@ define(['jquery', 'underscore', 'backbone', 'templates', '../models/application-
         initialize: function() {
             this.model = new model.OSDModel();
             this.listenTo(this.model, 'change', this.render);
+            _.bindAll(this, 'animationFinished');
+        },
+        animationFinished: function() {
+            if (this.$el.hasClass('part1')) {
+                console.log('done with part1');
+                this.$el.html(this.template(this.model.toJSON()));
+                this.$el.removeClass().addClass('fadeInAnim part2');
+            } else if (this.$el.hasClass('part2')) {
+                this.$el.removeClass();
+                this.$el.off('webkitAnimationEnd', this.animationFinished);
+            }
         },
         render: function() {
-//          console.log(this.model.toJSON());
-            this.$el.html(this.template(this.model.toJSON()));
+            //          console.log(this.model.toJSON());
+            this.$el.on('webkitAnimationEnd', this.animationFinished);
+            this.$el.addClass('part1 fadeOutAnim');
         }
     });
 
