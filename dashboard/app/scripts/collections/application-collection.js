9981b891b1c9bd08dd87a88f0951cccfabc2ca57
Yan-Fa Li <yanfali@gmail.com>
2013-07-22 11:09:29 -0700

    Teach OSD collection model about API endpoint

--- dashboard/app/scripts/collections/application-collection.js
+++ dashboard/app/scripts/collections/application-collection.js
@@ -1,14 +1,23 @@
+/*jshint -W106*/
 /*global define*/
 
-define([
-    'underscore',
-    'backbone',
-    'models/application-model'
-], function (_, Backbone, models) {
+define(['underscore', 'backbone', 'models/application-model'], function(_, Backbone, models) {
     'use strict';
 
     var OSDCollection = Backbone.Collection.extend({
+        cluster: 1,
+        epoch: 1,
+        added_ms: 0,
+        url: function() {
+            return '/api/v1/cluster/' + this.cluster + '/osd';
+        },
+        parse: function(response) {
+            this.epoch = response.epoch;
+            this.added_ms = response.added_ms;
+            return response.osds;
+        },
         model: models.OSDModel
+
     });
 
     return OSDCollection;
