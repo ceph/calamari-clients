f0e3762bce417ccce6fa386e542934597195414f
Yan-Fa Li <yanfali@gmail.com>
2013-08-05 16:00:42 -0700

    Use modernizr to detect the animation prefix

--- dashboard/app/scripts/helpers/animation.js
+++ dashboard/app/scripts/helpers/animation.js
@@ -1,8 +1,16 @@
-/*global define*/
+/*global define, Modernizr*/
 
 'use strict';
 define(['jquery'], function($) {
-    var events = 'webkitAnimationEnd animationend';
+    var animationEndEventNames = {
+        'WebkitAnimation': 'webkitAnimationEnd',
+        'MozAnimation': 'animationEnd',
+        'OAnimation': 'oanimationend',
+        'msAnimation': 'MSAnimationEnd',
+        'animation': 'animationend'
+    },
+        animationEndEvent = animationEndEventNames[Modernizr.prefixed('animation')];
+
     // Returns a function which animates using the specified CSS3 class1 and class2.
     // Signature of return function is:
     //   selector - jquery selector,
@@ -14,22 +22,22 @@ define(['jquery'], function($) {
     // @param class2 - css class of animation 2
     // @returns jQuery promise
     function pair(class1, class2) {
-        return function(selector, fn1, fn2) {
+        return function($selector, fn1, fn2) {
             var d = $.Deferred();
-            selector.on(events, d.resolve);
+            $selector.on(animationEndEvent, d.resolve);
             var self = this;
-            selector.addClass(class1);
+            $selector.addClass(class1);
             return d.promise().then(function() {
                 if (fn1) {
                     fn1.apply(self);
                 }
-                selector.off(events, d.resolve).removeClass(class1);
+                $selector.off(animationEndEvent, d.resolve).removeClass(class1);
                 d = $.Deferred();
-                selector.on(events, d.resolve).addClass(class2);
+                $selector.on(animationEndEvent, d.resolve).addClass(class2);
                 return d.promise();
             }).then(function() {
-                selector.removeClass(class2);
-                selector.off(events, d.resolve);
+                $selector.removeClass(class2);
+                $selector.off(animationEndEvent, d.resolve);
                 if (fn2) {
                     fn2.apply(self);
                 }
@@ -40,14 +48,14 @@ define(['jquery'], function($) {
     function single(class1) {
         return function(selector, fn1) {
             var d = $.Deferred();
-            selector.on(events, d.resolve);
+            selector.on(animationEndEvent, d.resolve);
             var self = this;
             selector.addClass(class1);
             return d.promise().then(function() {
+                selector.off(animationEndEvent, d.resolve).removeClass(class1);
                 if (fn1) {
                     fn1.apply(self);
                 }
-                selector.off(events, d.resolve).removeClass(class1);
             });
         };
     }
