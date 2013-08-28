873817b586830d4c410f445a76eefb2b3faf5015
Yan-Fa Li <yanfali@gmail.com>
2013-08-28 00:37:45 -0700

    Workaround - add more slots for graphs
    - Add slots for up to 30 graphs
    - Add Missing graphite template

--- dashboard/app/scripts/views/graphwall-view.js
+++ dashboard/app/scripts/views/graphwall-view.js
@@ -18,7 +18,7 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'helpers/graph-utils', 
             this.makeCPUGraphUrl = gutils.makeGraphURL('png', this.baseUrl, this.heightWidth, this.cpuTargets);
             this.osdOpLatencyTargets = gutils.makeTargets(gutils.makeOpLatencyTargets(['op_r_latency', 'op_w_latency', 'op_rw_latency']));
             this.makeOpsLatencyGraphUrl = gutils.makeGraphURL('png', this.baseUrl, this.heightWidth, this.osdOpLatencyTargets);
-            this.journalOpsTargets = gutils.makeTargets(gutils.makeFilestoreTargets(['journal_ops','journal_wr']));
+            this.journalOpsTargets = gutils.makeTargets(gutils.makeFilestoreTargets(['journal_ops', 'journal_wr']));
             this.makeJournalOpsGraphUrl = gutils.makeGraphURL('png', this.baseUrl, this.heightWidth, this.journalOpsTargets);
         },
         makeHostUrls: function() {
@@ -28,13 +28,14 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'helpers/graph-utils', 
                 return fn(host);
             });
         },
-        selectors: ['0-1', '0-2', '0-3', '1-1', '1-2', '1-3', '2-1', '2-2', '2-3'],
+        selectors: ['0-1', '0-2', '0-3', '1-1', '1-2', '1-3', '2-1', '2-2', '2-3', '3-1', '3-2', '3-3', '4-1', '4-2', '4-3', '5-1', '5-2', '5-3', '6-1', '6-2', '6-3', '7-1', '7-2', '7-3', '8-1', '8-2', '8-3', '9-1', '9-2', '9-3', '10-1', '10-2', '10-3'],
         populateAll: function() {
             var urls = this.makeHostUrls();
             var self = this;
             this.ui.title.text('CPU Load for Cluster');
             _.each(urls, function(url, index) {
                 var $graph = self.$('.graph' + self.selectors[index]);
+                $graph.css('visibility', 'visible');
                 $graph.html('<img src=' + url + '>');
             });
         }
