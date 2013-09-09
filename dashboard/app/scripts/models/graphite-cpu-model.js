9a38fe818b10ed42f465beb8ef1fa86e3b922827
Yan-Fa Li <yanfali@gmail.com>
2013-09-09 13:34:40 -0700

    Update cpu-model to be configurable by caller

--- dashboard/app/scripts/models/graphite-cpu-model.js
+++ dashboard/app/scripts/models/graphite-cpu-model.js
@@ -2,17 +2,27 @@
 
 define([
     'underscore',
-    'backbone'
+    'backbone',
+    'marionette'
 ], function (_, Backbone) {
     'use strict';
 
     var GraphiteCpuModel = Backbone.Model.extend({
-        url: '/api/graphite/cpumetrics',
+        url: function() {
+            return this.graphiteHost + '/metrics/find?query=servers.' + this.host + '.cpu.*';
+        },
         parse: function(resp) {
             return _.reduce(resp, function(memo, value) {
                 memo[value.text] = value.id;
                 return memo;
             }, {});
+        },
+        initialize: function(attrs, options) {
+            this.graphiteHost = Backbone.Marionette.getOption(options, 'graphiteHost');
+        },
+        fetchMetrics: function(host) {
+            this.host = host;
+            return this.fetch();
         },
         defaults: {
         }
