770b16a8697e40893b170edde27bdd45adebbf8c
Yan-Fa Li <yanfali@gmail.com>
2013-09-09 17:26:51 -0700

    Add helpers to get cpu and devices from models
    Filter out the CPU and Device data to get the
    keys we need to build our graphs.

--- dashboard/app/scripts/models/graphite-cpu-model.js
+++ dashboard/app/scripts/models/graphite-cpu-model.js
@@ -1,15 +1,16 @@
 /*global define*/
 
-define([
-    'underscore',
-    'backbone',
-    'models/graphite-model',
-], function (_, Backbone, GraphiteModel) {
+define(['underscore', 'backbone', 'models/graphite-model', ], function(_, Backbone, GraphiteModel) {
     'use strict';
 
     var GraphiteCpuModel = GraphiteModel.extend({
         url: function() {
             return this.graphiteHost + '/metrics/find?query=servers.' + this.host + '.cpu.*';
+        },
+        cpuList: function() {
+            return _.map(this.attributes, function(v, k) {
+                return k;
+            });
         }
     });
 
--- dashboard/app/scripts/models/graphite-iostat-model.js
+++ dashboard/app/scripts/models/graphite-iostat-model.js
@@ -1,15 +1,20 @@
 /*global define*/
 
-define([
-    'underscore',
-    'backbone',
-    'models/graphite-model'
-], function (_, Backbone, GraphiteModel) {
+define(['underscore', 'backbone', 'models/graphite-model'], function(_, Backbone, GraphiteModel) {
     'use strict';
 
     var GraphiteIoModel = GraphiteModel.extend({
         url: function() {
             return this.graphiteHost + '/metrics/find?query=servers.' + this.host + '.iostat.*';
+        },
+        DriveRegexp: new RegExp('sd[a-z]$'),
+        listDevices: function() {
+            var re = this.DriveRegexp;
+            return _.filter(_.map(this.attributes, function(v, k) {
+                return k;
+            }), function(v) {
+                return re.test(v);
+            });
         }
     });
 
