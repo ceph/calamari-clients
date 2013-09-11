72ab85bc241d03edd6dfe56a53f3a521ecf0c344
Yan-Fa Li <yanfali@gmail.com>
2013-09-11 10:11:51 -0700

    Make overview active by default
    - add more rows mechanically for now

--- dashboard/app/scripts/views/graphwall-view.js
+++ dashboard/app/scripts/views/graphwall-view.js
@@ -159,7 +159,7 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'helpers/graph-utils', 
                 });
             };
         },
-        selectors: ['0-1', '0-2', '1-1', '1-2', '2-1', '2-2', '3-1', '3-2', '4-1', '4-2'],
+        selectors: ['0-1', '0-2', '1-1', '1-2', '2-1', '2-2', '3-1', '3-2', '4-1', '4-2', '5-1', '5-2', '6-1', '6-2', '7-1', '7-2', '8-1', '8-2'],
         imageLoader: function($el, url) {
             setTimeout(function() {
                 $el.html('<i class="icon-spinner icon-spin icon-large icon-3x"></i>');
