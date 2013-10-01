3f162b23006aeec47ab4e150031fd5f591003842
Yan-Fa Li <yanfali@gmail.com>
2013-10-01 16:29:51 -0700

    Handle Server Side Errors
    - Server Timeouts
    - Session Timed out (401)
    - Server Errors (500>)
    - Everything Else
    - JSON Parser Errors

--- dashboard/app/scripts/views/alerts.js
+++ dashboard/app/scripts/views/alerts.js
@@ -4,12 +4,19 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'marionette'], function
     'use strict';
 
     var AlertsView = Backbone.Marionette.ItemView.extend({
+        serverErrorTemplate: _.template('Server Error (<%- statusCode %>) <%- responseText %>. Please contact Administrator.'),
+        unexpectedErrorTemplate: _.template('Unexpected Error (<%- statusCode %>)<%- responseText %>. Please contact Administrator.'),
+        parserErrorTemplate: _.template('Error decoding <%- source %> response from server. Please contact Administrator.'),
+        template: JST['app/scripts/templates/alerts.ejs'],
         throttleMs: 10000,
         throttleCount: 3,
         initialize: function() {
             this.App = Backbone.Marionette.getOption(this, 'App');
             this.listenTo(this.App.vent, 'app:neterror', this.neterrorHandler);
-            this.error = _.after(this.throttleCount, _.throttle(this.error, this.throttleMs));
+            _.each(['timeout', 'sessionExpired', 'serverError', 'parserError'], function(fnName) {
+                this[fnName] = _.throttle(this[fnName], this.throttleMs);
+            }, this);
+            this.timeout = _.after(this.throttleCount, this.timeout);
             _.bindAll(this, 'neterrorHandler');
         },
         notyDefaults: {
@@ -19,21 +26,63 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'marionette'], function
         timeoutCount: 0,
         error: function(msg) {
             noty(msg);
+        },
+        warning: function(msg) {
+            msg.type = 'warning';
+            noty(msg);
+        },
+        timeout: function(msg) {
+            msg.text = 'Dashboard Update Timed Out. Please check your connection.';
+            this.timeoutCount++;
+            this.error(msg);
             console.log('timeout count ' + this.timeoutCount);
+        },
+        sessionExpired: function(msg) {
+            msg.text = 'Session Has Timed Out. Please Login again.';
+            msg.buttons = [{
+                addClass: 'btn btn-primary',
+                text: 'Login',
+                onClick: function($noty) {
+                    $noty.close();
+                    window.location = '/login';
+                }
+            }];
+            this.warning(msg);
+        },
+        serverError: function(msg, xhr) {
+            msg.text = this.unexpectedErrorTemplate(xhr);
+            this.error(msg);
+        },
+        parserError: function(msg, xhr) {
+            msg.text = this.parserErrorTemplate(xhr);
+            msg.timeout = 10000;
+            this.error(msg);
         },
         neterrorHandler: function(source, xhr) {
             var errorType = xhr.statusText;
-
-            if (errorType) {
-                var msg = _.extend({}, this.notyDefaults);
-                if (errorType === 'timeout') {
-                    msg.text = 'Dashboard Update Timed Out. Please check your connection.';
-                    this.timeoutCount++;
-                    this.error(msg);
-                }
+            if (!errorType) {
+                return;
             }
-        },
-        template: JST['app/scripts/templates/alerts.ejs']
+            var msg = _.extend({}, this.notyDefaults);
+            if (errorType === 'timeout') {
+                return this.timeout(msg);
+            }
+            if (errorType === 'error') {
+                if (xhr.status === 401) {
+                    return this.sessionExpired(msg);
+                }
+                if (xhr.status >= 500) {
+                    return this.serverError(msg, xhr);
+                }
+                return this.unexpectedError(msg, xhr);
+            }
+            // parsererror doesn't seem to work consistently
+            if (errorType === 'parsererror' || (xhr.status === 200 && errorType === 'OK')) {
+                return this.parserError(msg, _.extend({
+                    source: source
+                }, xhr));
+            }
+        }
     });
 
     return AlertsView;
