8efac3694a709a3298653d96f9dbf36dfb598294
Yan-Fa Li <yanfali@gmail.com>
2013-11-18 14:56:41 -0800

    Define layouts for different numbers of PGs
    Layouts for 15k, 30k, 60k and 100k PGs.
    Includes scaling factors, and precalculated height & widths based on a
    maximum layout of 400pxx250px. Scale factor scales to roughly the same
    dimensions.

--- dashboard/app/scripts/views/pgmap-view.js
+++ dashboard/app/scripts/views/pgmap-view.js
@@ -14,7 +14,37 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'helpers/gauge-helper',
             this.listenToOnce(this, 'render', this.postRender);
             gaugeHelper(this);
         },
+        count: 15000,
+        getLayout: function(count) {
+            var width = 400,
+                height = 250,
+                x = 10,
+                y = 10,
+                scale = 1;
+            if (count <= 15000) {
+                width = 155;
+                height = 95;
+                scale = 2.58
+            } else if (count <= 30000) {
+                width = 220;
+                height = 138;
+                scale = 1.8;
+            } else if (count <= 60000) {
+                width = 310;
+                height = 194;
+                scale = 1.29;
+            }
+            x = y = x / scale;
+            return {
+                x: x,
+                y: y,
+                width: width,
+                height: height,
+                scale: scale
+            };
+        },
         postRender: function() {
+            var layout = this.getLayout(this.count);
             var width = this.ui.container.width();
             var height = this.ui.container.height();
             this.stage = new Kinetic.Stage({
@@ -26,15 +56,17 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'helpers/gauge-helper',
             this.layer = new Kinetic.Layer();
 
             this.background = new Kinetic.Rect({
-                x: 0,
-                y: 0,
-                width: this.stage.getWidth(),
-                height: this.stage.getHeight(),
-                fill: '#fff'
+                x: layout.x,
+                y: layout.y,
+                width: layout.width,
+                height: layout.height,
+                fill: 'red'
             });
 
             this.layer.add(this.background);
             this.stage.add(this.layer);
+            this.background.getContext().scale(layout.scale, layout.scale);
+            this.layer.drawScene();
 
 
         }
