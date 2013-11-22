3d9bcd767fd3b493d4cb24a027a152e0c8fcd9a8
Yan-Fa Li <yanfali@gmail.com>
2013-11-21 22:07:30 -0800

    OSD Widget implementation
    - add event handlers for update
    - calc percentage of down OSDs

--- dashboard/app/scripts/views/mon-dash-view.js
+++ dashboard/app/scripts/views/mon-dash-view.js
@@ -6,6 +6,10 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'helpers/gauge-helper',
     var OsdDashView = Backbone.Marionette.ItemView.extend({
         className: 'col-lg-3 col-md-3 col-sm-6 col-xs-6 custom-gutter',
         template: JST['app/scripts/templates/mon-dash.ejs'],
+        ui: {
+            'headline': '.headline',
+            'subtext': '.subtext'
+        },
         initialize: function() {
             this.App = Backbone.Marionette.getOption(this, 'App');
             gaugeHelper(this);
--- dashboard/app/scripts/views/osd-dash-view.js
+++ dashboard/app/scripts/views/osd-dash-view.js
@@ -6,9 +6,59 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'helpers/gauge-helper',
     var OsdDashView = Backbone.Marionette.ItemView.extend({
         className: 'col-lg-3 col-md-3 col-sm-6 col-xs-6 custom-gutter',
         template: JST['app/scripts/templates/osd-dash.ejs'],
+        headlineTemplate: _.template('<%- ok %>/<%- total %>'),
+        subtextTemplate: _.template('<%- percentage %>% out'),
+        ui: {
+            'headline': '.headline',
+            'subline': '.subline',
+            'subtext': '.subtext'
+        },
+        modelEvents: {
+            'change': 'modelChanged'
+        },
         initialize: function() {
+            _.bindAll(this, 'set', 'modelChanged');
             this.App = Backbone.Marionette.getOption(this, 'App');
+            if (this.App) {
+                this.listenTo(this.App.vent, 'status:update', this.set);
+            }
+            this.model = new Backbone.Model();
             gaugeHelper(this);
+        },
+        set: function(model) {
+            var attr = model.attributes.osd;
+            console.log(model.attributes);
+            this.model.set({
+                ok: attr.ok.count,
+                warn: attr.warn.count,
+                critical: attr.critical.count
+            });
+        },
+        modelChanged: function(model) {
+            var ok = model.get('ok');
+            var warn = model.get('warn');
+            var critical = model.get('critical');
+            var count = 0;
+            if (_.isNumber(ok)) {
+                count += ok;
+            }
+            if (_.isNumber(warn)) {
+                count += warn;
+            }
+            if (_.isNumber(critical)) {
+                count += critical;
+            }
+            this.ui.subline.text('In and Up');
+            if (warn || critical) {
+                var percentage = Math.floor(((warn + critical) / count) * 100);
+                this.ui.subtext.text(this.subtextTemplate({
+                    percentage: percentage
+                }));
+            }
+            this.ui.headline.text(this.headlineTemplate({
+                ok: ok,
+                total: count
+            }));
         }
     });
 
