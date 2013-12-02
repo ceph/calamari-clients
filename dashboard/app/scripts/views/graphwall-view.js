b89c782568df75702b713aae01aa7a90c3267bec
Yan-Fa Li <yanfali@gmail.com>
2013-12-02 15:41:46 -0800

    Test DyGraph Code

--- dashboard/app/scripts/views/graphwall-view.js
+++ dashboard/app/scripts/views/graphwall-view.js
@@ -1,6 +1,6 @@
 /*global define*/
 
-define(['jquery', 'underscore', 'backbone', 'templates', 'helpers/graph-utils', 'models/application-model', 'marionette'], function($, _, Backbone, JST, gutils, models) {
+define(['jquery', 'underscore', 'backbone', 'templates', 'helpers/graph-utils', 'models/application-model', 'dygraphs', 'marionette'], function($, _, Backbone, JST, gutils, models, Dygraph) {
     'use strict';
 
     var GraphwallView = Backbone.Marionette.ItemView.extend({
@@ -36,69 +36,70 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'helpers/graph-utils', 
             });
         },
         graphs: [{
-            metrics: ['byte_avail', 'byte_free', 'byte_used'],
-            fn: 'makeDiskSpaceBytesGraphUrl',
-            util: 'makeDiskSpaceTargets'
-        }, {
-            metrics: ['inodes_avail', 'inodes_free', 'inodes_used'],
-            fn: 'makeDiskSpaceInodesGraphUrl',
-            util: 'makeDiskSpaceTargets'
-        }, {
-            metrics: ['system', 'user', 'idle'],
-            fn: 'makeCPUGraphUrl',
-            util: 'makeCPUTargets'
-        }, {
-            metrics: ['system', 'user', 'nice', 'idle', 'iowait', 'irq', 'softirq', 'steal'],
-            fn: 'makeCPUDetailGraphUrl',
-            util: 'makeCPUDetailedTargets'
-        }, {
-            metrics: ['op_r_latency', 'op_w_latency', 'op_rw_latency'],
-            fn: 'makeOpsLatencyGraphUrl',
-            util: 'makeOpLatencyTargets'
-        }, {
-            metrics: ['journal_ops', 'journal_wr'],
-            fn: 'makeJournalOpsGraphUrl',
-            util: 'makeFilestoreTargets'
-        }, {
-            metrics: ['01', '05', '15'],
-            fn: 'makeLoadAvgGraphUrl',
-            util: 'makeLoadAvgTargets'
-        }, {
-            metrics: ['Active', 'Buffers', 'Cached', 'MemFree'],
-            fn: 'makeMemoryGraphUrl',
-            util: 'makeMemoryTargets'
-        }, {
-            metrics: ['read_byte_per_second', 'write_byte_per_second'],
-            fn: 'makeHostDeviceRWBytesGraphUrl',
-            util: 'makeIOStatIOPSTargets'
-        }, {
-            metrics: ['read_await', 'write_await'],
-            fn: 'makeHostDeviceRWAwaitGraphUrl',
-            util: 'makeIOStatIOPSTargets'
-        }, {
-            metrics: ['iops'],
-            fn: 'makeHostDeviceIOPSGraphUrl',
-            util: 'makeIOStatIOPSTargets'
-        }, {
-            metrics: ['rx_byte', 'tx_byte'],
-            fn: 'makeHostNetworkTXRXBytesGraphURL',
-            util: 'makeNetworkTargets'
-        }, {
-            metrics: ['tx_packets', 'rx_packets'],
-            fn: 'makeHostNetworkTXRXPacketsGraphURL',
-            util: 'makeNetworkTargets'
-        }, {
-            metrics: ['tx_errors', 'rx_errors'],
-            fn: 'makeHostNetworkTXRXErrorsGraphURL',
-            util: 'makeNetworkTargets'
-        }, {
-            metrics: ['tx_drop', 'rx_drop'],
-            fn: 'makeHostNetworkTXRXDropGraphURL',
-            util: 'makeNetworkTargets'
-        }],
+                metrics: ['byte_avail', 'byte_free', 'byte_used'],
+                fn: 'makeDiskSpaceBytesGraphUrl',
+                util: 'makeDiskSpaceTargets'
+            }, {
+                metrics: ['inodes_avail', 'inodes_free', 'inodes_used'],
+                fn: 'makeDiskSpaceInodesGraphUrl',
+                util: 'makeDiskSpaceTargets'
+            }, {
+                metrics: ['system', 'user', 'idle'],
+                fn: 'makeCPUGraphUrl',
+                util: 'makeCPUTargets'
+            }, {
+                metrics: ['system', 'user', 'nice', 'idle', 'iowait', 'irq', 'softirq', 'steal'],
+                fn: 'makeCPUDetailGraphUrl',
+                util: 'makeCPUDetailedTargets'
+            }, {
+                metrics: ['op_r_latency', 'op_w_latency', 'op_rw_latency'],
+                fn: 'makeOpsLatencyGraphUrl',
+                util: 'makeOpLatencyTargets'
+            }, {
+                metrics: ['journal_ops', 'journal_wr'],
+                fn: 'makeJournalOpsGraphUrl',
+                util: 'makeFilestoreTargets'
+            }, {
+                metrics: ['01', '05', '15'],
+                fn: 'makeLoadAvgGraphUrl',
+                util: 'makeLoadAvgTargets'
+            }, {
+                metrics: ['Active', 'Buffers', 'Cached', 'MemFree'],
+                fn: 'makeMemoryGraphUrl',
+                util: 'makeMemoryTargets'
+            }, {
+                metrics: ['read_byte_per_second', 'write_byte_per_second'],
+                fn: 'makeHostDeviceRWBytesGraphUrl',
+                util: 'makeIOStatIOPSTargets'
+            }, {
+                metrics: ['read_await', 'write_await'],
+                fn: 'makeHostDeviceRWAwaitGraphUrl',
+                util: 'makeIOStatIOPSTargets'
+            }, {
+                metrics: ['iops'],
+                fn: 'makeHostDeviceIOPSGraphUrl',
+                util: 'makeIOStatIOPSTargets'
+            }, {
+                metrics: ['rx_byte', 'tx_byte'],
+                fn: 'makeHostNetworkTXRXBytesGraphURL',
+                util: 'makeNetworkTargets'
+            }, {
+                metrics: ['tx_packets', 'rx_packets'],
+                fn: 'makeHostNetworkTXRXPacketsGraphURL',
+                util: 'makeNetworkTargets'
+            }, {
+                metrics: ['tx_errors', 'rx_errors'],
+                fn: 'makeHostNetworkTXRXErrorsGraphURL',
+                util: 'makeNetworkTargets'
+            }, {
+                metrics: ['tx_drop', 'rx_drop'],
+                fn: 'makeHostNetworkTXRXDropGraphURL',
+                util: 'makeNetworkTargets'
+            }
+        ],
         makeGraphFunctions: function(options) {
             var targets = gutils.makeTargets(gutils[options.util](options.metrics));
-            this[options.fn] = gutils.makeGraphURL('png', this.baseUrl, this.heightWidth, targets);
+            this[options.fn] = gutils.makeGraphURL('json', this.baseUrl, this.heightWidth, targets);
         },
         initialize: function() {
             this.App = Backbone.Marionette.getOption(this, 'App');
@@ -218,7 +219,9 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'helpers/graph-utils', 
             };
         },
         selectTemplate: _.template('<select class="form-control" name="hosts"><option value="all" selected>Cluster - per host CPU</option><%= list %></select>'),
-        optionTemplate: _.template('<option value="<%- args.host %>">Host - <%- args.host %></option>"', null, { variable: 'args' }),
+        optionTemplate: _.template('<option value="<%- args.host %>">Host - <%- args.host %></option>"', null, {
+            variable: 'args'
+        }),
         renderHostSelector: function() {
             var hosts = this.App.ReqRes.request('get:hosts');
             var opts = _.reduce(hosts, function(memo, host) {
@@ -233,7 +236,7 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'helpers/graph-utils', 
         },
         selectors: ['0-1', '0-2', '1-1', '1-2', '2-1', '2-2', '3-1', '3-2', '4-1', '4-2', '5-1', '5-2', '6-1', '6-2', '7-1', '7-2', '8-1', '8-2'],
         imageLoader: function($el, url) {
-            setTimeout(function() {
+            _.defer(function() {
                 $el.html('<i class="fa fa-spinner fa-spin fa-lg fa-3x"></i>');
                 var image = new Image();
                 image.src = url;
@@ -243,7 +246,19 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'helpers/graph-utils', 
                 image.onerror = function() {
                     $el.html('<i class="fa fa-exclamation-triangle fa-lg fa-3x"></i>');
                 };
-            }, 0);
+            });
+        },
+        dygraphLoader: function($el, url) {
+            var self = this;
+            _.defer(function() {
+                $el.html('<i class="fa fa-spinner fa-spin fa-lg fa-3x"></i>');
+                $.ajax({url: url, dataType: 'json'}).done(function(resp) {
+                    var post = self.processDygraph(resp);
+                    new Dygraph($el[0], post.data, {
+                        labels: post.labels
+                    });
+                });
+            });
         },
         makeHostOverviewGraphUrl: function(host) {
             return function() {
@@ -253,6 +268,40 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'helpers/graph-utils', 
         hideGraphs: function() {
             this.$('.graph-card').css('visibility', 'hidden');
         },
+        processDygraph: function(resp) {
+            var data = _.map(resp, function(value) {
+                var label = value.target;
+                var points = _.reduce(value.datapoints, function(memo, values) {
+                    memo[0].push(new Date(values[1] * 1000));
+                    memo[1].push(values[0]);
+                    return memo;
+                }, [
+                    [],
+                    []
+                ]);
+                return {
+                    label: label,
+                    points: points
+                };
+            });
+            data = _.reduce(data, function(memo, value, index) {
+                memo.labels.push(value.label);
+                if (index === 0) {
+                    memo.data = _.zip(value.points[0], value.points[1]);
+                } else {
+                    memo.data = _.map(memo.data, function(innerValue, innerIndex) {
+                        var v = value.points[1][innerIndex];
+                        innerValue.push(v);
+                        return innerValue;
+                    });
+                }
+                return memo;
+            }, {
+                labels: ['Date'],
+                data: []
+            });
+            return data;
+        },
         populateAll: function(title, fn) {
             var urls = fn.call(this);
             var self = this;
@@ -260,7 +309,7 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'helpers/graph-utils', 
             _.each(urls, function(url, index) {
                 var $graph = self.$('.graph' + self.selectors[index]);
                 $graph.css('visibility', 'visible');
-                self.imageLoader($graph, url);
+                self.dygraphLoader($graph, url);
             });
         }
     });
