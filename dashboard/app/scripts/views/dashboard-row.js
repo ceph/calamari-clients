6701b7fb2a2362f6eec605cc7bc5ad18bdd2d292
Yan-Fa Li <yanfali@gmail.com>
2014-01-08 15:06:11 -0800

    Prototype React Row Class
    - mixed in Backbone.Events object to TypeOne widget
    - added listeners for various application events
    - Experimenting with adding widgets programmatically using React

---
+++ dashboard/app/scripts/views/dashboard-row.js
@@ -0,0 +1,34 @@
+/* global define */
+define(['react'], function(React) {
+    'use strict';
+
+    var DashboardRow = React.createClass({
+        getInitialState: function() {
+            return {
+                one: '',
+                two: '',
+                three: '',
+                four: ''
+            };
+        },
+        render: function() {
+            return React.DOM.div({
+                className: 'row gauges'
+            }, [
+                React.DOM.div({
+                    className: 'one'
+                }, this.props.one),
+                React.DOM.div({
+                    className: 'two'
+                }, this.props.two),
+                React.DOM.div({
+                    className: 'three',
+                }, this.props.three),
+                React.DOM.div({
+                    className: 'four'
+                }, this.props.four)
+            ]);
+        }
+    });
+    return DashboardRow;
+});
