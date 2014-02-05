6d7fbf8dff2c401f8c8e779d7c5a09610ba30631
Yan-Fa Li <yanfali@gmail.com>
2014-02-05 14:24:33 -0800

    Register services with Module

--- manage/app/scripts/app.js
+++ manage/app/scripts/app.js
@@ -1,7 +1,7 @@
 /* global define */
 (function() {
     'use strict';
-    define(['angular', 'RouteConfig', 'controllers/root', 'controllers/pool', 'controllers/osd', 'controllers/osd-host', 'angular-cookies', 'angular-resource', 'angular-sanitize', 'angular-route', 'angular-strap', 'angular-animate', 'restangular'], function(angular, RouteConfig, RootController, PoolController, OSDController, OSDHostController) {
+    define(['angular', 'RouteConfig', 'controllers/root', 'controllers/pool', 'controllers/osd', 'controllers/osd-host', 'services/cluster', 'services/pool', 'services/server', 'services/key', 'angular-cookies', 'angular-resource', 'angular-sanitize', 'angular-route', 'angular-strap', 'angular-animate', 'restangular'], function(angular, RouteConfig, RootController, PoolController, OSDController, OSDHostController, ClusterService, PoolService, ServerService, KeyService) {
         angular.module('manageApp', [
                 'ngCookies',
                 'ngResource',
@@ -15,6 +15,10 @@
             .controller('PoolController', PoolController)
             .controller('OSDController', OSDController)
             .controller('OSDHostController', OSDHostController)
+            .factory('ClusterService', ClusterService)
+            .factory('PoolService', PoolService)
+            .factory('ServerService', ServerService)
+            .factory('KeyService', KeyService)
             .config(RouteConfig);
         angular.bootstrap(document.getElementsByClassName('manageApp')[0], ['manageApp']);
     });
