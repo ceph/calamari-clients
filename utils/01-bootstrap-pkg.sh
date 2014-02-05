b4880978ae39d27ce1eb81c90b84c4165b3d84e7
Yan-Fa Li <yanfali@gmail.com>
2014-02-05 14:09:20 -0800

    Add ZSH

--- utils/01-bootstrap-pkg.sh
+++ utils/01-bootstrap-pkg.sh
@@ -1,2 +1,4 @@
-sudo apt-get install vim tmux
+sudo apt-get -y install vim tmux zsh
+wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
 sudo salt-call state.highstate
+
