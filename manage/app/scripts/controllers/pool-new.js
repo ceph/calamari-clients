7133f2b9d218623b3409e6c413feba72201b5b46
Yan-Fa Li <yanfali@gmail.com>
2014-02-07 11:55:50 -0800

    Issue #7297 Crush rule set and rule handling
    Take into account size and which subrule is active
    
    Rule Sets contain n rules. Which rule is active is based on the
    replication value or size. Make the UI respond to changes and display
    the correct subrule name when changing the size.
    
    Handle the crush rule set changing
    
    - rename some dubiously named variables
    - fix a bug in the crushruleset generation, used the wrong index! DOH
    - reset size and rule label when the ruleset is changed

--- manage/app/scripts/controllers/pool-new.js
+++ manage/app/scripts/controllers/pool-new.js
@@ -2,55 +2,111 @@
 (function() {
     'use strict';
     define(['lodash', 'helpers/pool-helpers'], function(_, helpers) {
+        var poolDefaults = {
+            /* jshint camelcase:false */
+            name: '',
+            size: 2,
+            crush_ruleset: 0,
+            pg_num: 100
+        };
 
-        var PoolNewController = function($log, $q, $scope, PoolService, ClusterService, CrushService, ToolService) {
-            var pool = {
-                /* jshint camelcase:false */
-                name: '',
-                size: 2,
-                crush_ruleset: 0,
-                pg_num: 100
-            };
-            $scope.pool = pool;
-            $scope.clusterName = ClusterService.clusterModel.name;
+        var PoolNewController = function($location, $log, $q, $scope, PoolService, ClusterService, CrushService, ToolService) {
             var self = this;
+            $scope.clusterName = ClusterService.clusterModel.name;
+            $scope.cancel = function() {
+                $location.path('/pool');
+            };
+            $scope.reset = function() {
+                $scope.name = '';
+                $scope.size = this.defaults.size;
+                $scope.crush_ruleset = this.defaults.crush_ruleset;
+                var osdcount = this.crushrulesets[this.defaults.crush_ruleset].osd_count;
+                $scope.pg_num = helpers.calculatePGNum(osdcount, $scope.size, this.mon_max_pool_pg_num);
+            };
+
             var promises = [PoolService.defaults(), CrushService.getList(), ToolService.config('mon_max_pool_pg_num')];
+
             $q.all(promises).then(function(results) {
+                /* jshint camelcase:false */
                 var result = _.chain(results);
                 self.defaults = result.shift().value();
                 self.crushrulesets = result.shift().value();
                 self.mon_max_pool_pg_num = parseInt(result.shift().value().value, 10);
-                /* jshint camelcase:false */
-                pool = _.extend(pool, {
+                var pool = _.extend(poolDefaults, {
                     size: self.defaults.size,
                     crush_ruleset: self.defaults.crush_ruleset,
                 });
 
                 $scope.defaults = self.defaults;
-                $scope.crushrulesets = self.crushrulesets;
-
-                $scope.$watch('pool.size', function(newValue /*, oldValue*/ ) {
-                    var ruleset = self.crushrulesets[pool.crush_ruleset];
-                    var limits = _.reduce(ruleset.rules, function(result, rule) {
+                $scope.crushraw = self.crushrulesets;
+                var crushruleSets = _.map(self.crushrulesets, function(set) {
+                    var rules = _.map(set.rules, function(rule, index) {
                         return {
-                            min: Math.min(rule.min_size, result.min),
-                            max: Math.max(rule.max_size, result.max)
+                            id: index,
+                            name: rule.name,
+                            min_size: rule.min_size,
+                            max_size: rule.max_size,
+                            osd_count: set.osd_count
+                        };
+                    });
+                    return {
+                        id: set.id,
+                        rules: rules,
+                        active_sub_rule: 0
+                    };
+                });
+                $scope.crushrulesets = crushruleSets;
+
+                $scope.name = pool.name;
+                $scope.size = pool.size;
+                $scope.crush_ruleset = pool.crush_ruleset;
+                $scope.pg_num = pool.pg_num;
+
+
+                $scope.$watch('size', function(newValue /*, oldValue*/ ) {
+                    var ruleset = crushruleSets[$scope.crush_ruleset];
+                    var limits = _.reduce(ruleset.rules, function(result, rule) {
+                        var active_rule = result.active_rule;
+                        if (newValue >= rule.min_size && newValue <= rule.max_size) {
+                            active_rule = rule.id;
+                        }
+                        return {
+                            min_size: Math.min(rule.min_size, result.min_size),
+                            max_size: Math.max(rule.max_size, result.max_size),
+                            active_rule: active_rule
                         };
                     }, {
-                        min: self.mon_max_pool_pg_num,
-                        max: 1
+                        min_size: self.mon_max_pool_pg_num,
+                        max_size: 1,
+                        active_rule: 0
                     });
-                    if (helpers.validateMaxMin.call(pool, 'size', newValue, limits.min, limits.max)) {
-                        var osdcount = self.crushrulesets[self.defaults.crush_ruleset].osd_count;
+                    $scope.limits = limits;
+                    if (helpers.validateMaxMin.call($scope, 'size', newValue, limits.min_size, limits.max_size)) {
+                        var osdcount = self.crushrulesets[$scope.crush_ruleset].rules[limits.active_rule].osd_count;
                         var pgnum = helpers.calculatePGNum(osdcount, newValue, self.mon_max_pool_pg_num);
-                        pool.pg_num = pgnum;
+                        $scope.pg_num = pgnum;
+                        $scope.crushrulesets[$scope.crush_ruleset].active_sub_rule = limits.active_rule;
                     }
                 });
-                $scope.$watch('pool.pg_num', function(newValue /*, oldValue*/ ) {
-                    helpers.validateMaxMin.call(pool, 'pg_num', newValue, 1, self.mon_max_pool_pg_num);
+                $scope.$watch('pg_num', function(newValue /*, oldValue*/ ) {
+                    helpers.validateMaxMin.call($scope, 'pg_num', newValue, 1, self.mon_max_pool_pg_num);
+                });
+                $scope.$watch('crush_ruleset', function(newValue, oldValue) {
+                    $scope.size = 2;
+                    crushruleSets[newValue].active_sub_rule = 0;
+                    crushruleSets[oldValue].active_sub_rule = 0;
                 });
             });
         };
-        return ['$log', '$q', '$scope', 'PoolService', 'ClusterService', 'CrushService', 'ToolService', PoolNewController];
+        return [
+            '$location',
+            '$log',
+            '$q',
+            '$scope',
+            'PoolService',
+            'ClusterService',
+            'CrushService',
+            'ToolService',
+            PoolNewController];
     });
 })();
--- manage/app/scripts/controllers/tools.js
+++ manage/app/scripts/controllers/tools.js
@@ -5,8 +5,10 @@
 
         var ToolController = function($scope, ClusterService, ToolService) {
             $scope.clusterName = ClusterService.clusterModel.name;
+            $scope.logui = 'spinner';
             ToolService.log().then(function(logs) {
                 $scope.logs = logs.lines.split('\n');
+                $scope.logui = 'logs';
             });
             ToolService.config().then(function(config) {
                 var sortedConfig = config.sort(function(a, b) {
