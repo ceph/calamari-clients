74cf56176303120e0ec364bf43a9592618b9236e
Yan-Fa Li <yanfali@gmail.com>
2014-02-10 11:01:52 -0800

    Tmuxinator first pass integration

--- utils/01-bootstrap-pkg.sh
+++ utils/01-bootstrap-pkg.sh
@@ -1,4 +1,6 @@
-sudo apt-get -y install vim tmux zsh
+sudo apt-get -y install vim tmux zsh ruby1.9.3
+mkdir -p ~/.tmuxinator
+cp -av /clients.git/utils/tmuxinator/session.yml ~/.tmuxinator
 wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
 sudo salt-call state.highstate
 
