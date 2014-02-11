c55d64dac1f14c238c76f86a493bdd4cf65e7829
Yan-Fa Li <yanfali@gmail.com>
2014-02-11 10:24:47 -0800

    Add section about install tmuxinator as root

--- utils/01-bootstrap-pkg.sh
+++ utils/01-bootstrap-pkg.sh
@@ -3,4 +3,15 @@ mkdir -p ~/.tmuxinator
 cp -av /clients.git/utils/tmuxinator/session.yml ~/.tmuxinator
 wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
 sudo salt-call state.highstate
+echo <<EOF
+Now you need install tmuxinator
 
+  sudo -s 
+  export REALLY_GEM_UPDATE_SYSTEM=true
+  gem update --system
+  gem install tmuxinator
+  exit
+
+  tmuxinator
+
+EOF
