48cfe46b989ff982179201ec40bd44052c755f86
Yan-Fa Li <yanfali@gmail.com>
2014-02-14 11:35:35 -0800

    Move pool business logic into pool-helpers
    Need to re-use a lot of this for pool-modify controller.
    Extract out the logic into common shared code.

--- manage/app/scripts/controllers/pool-new.js
+++ manage/app/scripts/controllers/pool-new.js
@@ -10,49 +10,13 @@
             pg_num: 100
         };
 
-        function getActiveRule(ruleset, maxPoolPgNum, size) {
-            /* jshint camelcase: false */
-            return _.reduce(ruleset.rules, function(result, rule) {
-                var active_rule = result.active_rule;
-                var osd_count = result.osd_count;
-                if (size >= rule.min_size && size <= rule.max_size) {
-                    active_rule = rule.id;
-                    osd_count = rule.osd_count;
-                }
-                return {
-                    min_size: Math.min(rule.min_size, result.min_size),
-                    max_size: Math.max(rule.max_size, result.max_size),
-                    active_rule: active_rule,
-                    osd_count: osd_count
-                };
-            }, {
-                min_size: maxPoolPgNum,
-                max_size: 1,
-                active_rule: 0,
-                osd_count: 0
-            });
-        }
-
         var PoolNewController = function($location, $log, $q, $scope, PoolService, ClusterService, CrushService, ToolService, RequestTrackingService, $modal) {
             var self = this;
             $scope.clusterName = ClusterService.clusterModel.name;
             $scope.cancel = function() {
                 $location.path('/pool');
             };
-            $scope.reset = function() {
-                var defaults = this.defaults;
-                $scope.pool.name = '';
-                $scope.pool.size = defaults.size;
-                $scope.pool.crush_ruleset = defaults.crush_ruleset;
-                var ruleset = this.crushrulesets[defaults.crush_ruleset];
-                var limits = getActiveRule(ruleset, defaults.mon_max_pool_pg_num, $scope.size);
-                var pgnum = helpers.calculatePGNum(limits.osd_count, $scope.size, defaults.mon_max_pool_pg_num);
-                if ($scope.pool.pg_num !== pgnum) {
-                    // Only reset pg num if it's different from calculated default
-                    // This catches where size isn't change but pg has been
-                    $scope.pool.pg_num = pgnum;
-                }
-            };
+            $scope.reset = helpers.makeReset($scope);
             $scope.create = function() {
                 if ($scope.poolForm.$invalid) {
                     return;
@@ -149,34 +113,7 @@
                     crush_ruleset: mergedDefaults.crush_ruleset,
                     pg_num: mergedDefaults.pg_num
                 };
-
-                $scope.$watch('pool.size', function(newValue /*, oldValue*/ ) {
-                    if (!_.isNumber(newValue)) {
-                        $scope.poolForm.size.$error.number = true;
-                        return;
-                    }
-                    var ruleset = crushruleSets[$scope.pool.crush_ruleset];
-                    var limits = getActiveRule(ruleset, $scope.defaults.mon_max_pool_pg_num, newValue);
-                    $scope.limits = limits;
-                    if (helpers.validateMaxMin.call($scope.pool, 'size', newValue, limits.min_size, limits.max_size)) {
-                        $scope.pool.pg_num = helpers.calculatePGNum(limits.osd_count, newValue, $scope.defaults.mon_max_pool_pg_num);
-                        $scope.crushrulesets[$scope.pool.crush_ruleset].active_sub_rule = limits.active_rule;
-                    }
-                });
-                $scope.$watch('pool.pg_num', function(newValue /*, oldValue*/ ) {
-                    if (!_.isNumber(newValue)) {
-                        $scope.poolForm.pg_num.$error.number = true;
-                        return;
-                    }
-                    $scope.poolForm.pg_num.$error.number = false;
-                    $scope.poolForm.pg_num.$pristine = true;
-                    helpers.validateMaxMin.call($scope.pool, 'pg_num', newValue, 1, $scope.defaults.mon_max_pool_pg_num);
-                });
-                $scope.$watch('pool.crush_ruleset', function(newValue, oldValue) {
-                    $scope.size = mergedDefaults.size;
-                    crushruleSets[newValue].active_sub_rule = 0;
-                    crushruleSets[oldValue].active_sub_rule = 0;
-                });
+                helpers.addWatches($scope);
             });
         };
         return [
--- manage/app/scripts/helpers/pool-helpers.js
+++ manage/app/scripts/helpers/pool-helpers.js
@@ -1,44 +1,121 @@
 /*global define */
 (function() {
     'use strict';
-    function roundUpToNextPowerOfTwo(num) {
-        // reference http://bits.stephan-brumme.com/roundUpToNextPowerOfTwo.html
-        /* jshint bitwise: false */
-        num--;
-        num |= num >> 1;
-        num |= num >> 2;
-        num |= num >> 4;
-        num |= num >> 8;
-        num |= num >> 16;
-        num++;
-        return num;
-    }
+    define(['lodash'], function(_) {
 
-    function calculatePGNum(osdcount, size, pgmax) {
-        var pgnum = roundUpToNextPowerOfTwo(osdcount * 100 / size);
-        if (pgnum > pgmax) {
-            pgnum = pgmax;
+        function roundUpToNextPowerOfTwo(num) {
+            // reference http://bits.stephan-brumme.com/roundUpToNextPowerOfTwo.html
+            /* jshint bitwise: false */
+            num--;
+            num |= num >> 1;
+            num |= num >> 2;
+            num |= num >> 4;
+            num |= num >> 8;
+            num |= num >> 16;
+            num++;
+            return num;
         }
-        return pgnum;
-    }
 
-    function validateMaxMin(fieldName, newValue, min, max) {
-        /* jshint validthis:true, camelcase: false*/
-        if (newValue < min) {
-            this[fieldName] = 1;
-            return false;
+        function calculatePGNum(osdcount, size, pgmax) {
+            var pgnum = roundUpToNextPowerOfTwo(osdcount * 100 / size);
+            if (pgnum > pgmax) {
+                pgnum = pgmax;
+            }
+            return pgnum;
         }
-        if (newValue > max) {
-            this[fieldName] = max;
-            return false;
+
+        function validateMaxMin(fieldName, newValue, min, max) {
+            /* jshint validthis:true, camelcase: false*/
+            if (newValue < min) {
+                this[fieldName] = 1;
+                return false;
+            }
+            if (newValue > max) {
+                this[fieldName] = max;
+                return false;
+            }
+            return true;
         }
-        return true;
-    }
-    define([], function() {
+
+        function getActiveRule(ruleset, maxPoolPgNum, size) {
+            /* jshint camelcase: false */
+            return _.reduce(ruleset.rules, function(result, rule) {
+                var active_rule = result.active_rule;
+                var osd_count = result.osd_count;
+                if (size >= rule.min_size && size <= rule.max_size) {
+                    active_rule = rule.id;
+                    osd_count = rule.osd_count;
+                }
+                return {
+                    min_size: Math.min(rule.min_size, result.min_size),
+                    max_size: Math.max(rule.max_size, result.max_size),
+                    active_rule: active_rule,
+                    osd_count: osd_count
+                };
+            }, {
+                min_size: maxPoolPgNum,
+                max_size: 1,
+                active_rule: 0,
+                osd_count: 0
+            });
+        }
+
+        function makeReset($scope) {
+            /* jshint camelcase: false */
+            return function() {
+                var defaults = $scope.defaults;
+                $scope.pool.name = '';
+                $scope.pool.size = defaults.size;
+                $scope.pool.crush_ruleset = defaults.crush_ruleset;
+                var ruleset = $scope.crushrulesets[defaults.crush_ruleset];
+                var limits = getActiveRule(ruleset, defaults.mon_max_pool_pg_num, $scope.size);
+                var pgnum = calculatePGNum(limits.osd_count, $scope.size, defaults.mon_max_pool_pg_num);
+                if ($scope.pool.pg_num !== pgnum) {
+                    // Only reset pg num if it's different from calculated default
+                    // This catches where size isn't change but pg has been
+                    $scope.pool.pg_num = pgnum;
+                }
+            };
+        }
+
+        /* Business Logic for crush rule sets pool replicas and placement groups */
+        function addWatches($scope) {
+            /* jshint camelcase: false */
+            $scope.$watch('pool.size', function(newValue /*, oldValue*/ ) {
+                if (!_.isNumber(newValue)) {
+                    $scope.poolForm.size.$error.number = true;
+                    return;
+                }
+                var ruleset = $scope.crushrulesets[$scope.pool.crush_ruleset];
+                var limits = getActiveRule(ruleset, $scope.defaults.mon_max_pool_pg_num, newValue);
+                $scope.limits = limits;
+                if (validateMaxMin.call($scope.pool, 'size', newValue, limits.min_size, limits.max_size)) {
+                    $scope.pool.pg_num = calculatePGNum(limits.osd_count, newValue, $scope.defaults.mon_max_pool_pg_num);
+                    $scope.crushrulesets[$scope.pool.crush_ruleset].active_sub_rule = limits.active_rule;
+                }
+            });
+            $scope.$watch('pool.pg_num', function(newValue /*, oldValue*/ ) {
+                if (!_.isNumber(newValue)) {
+                    $scope.poolForm.pg_num.$error.number = true;
+                    return;
+                }
+                $scope.poolForm.pg_num.$error.number = false;
+                $scope.poolForm.pg_num.$pristine = true;
+                validateMaxMin.call($scope.pool, 'pg_num', newValue, 1, $scope.defaults.mon_max_pool_pg_num);
+            });
+            $scope.$watch('pool.crush_ruleset', function(newValue, oldValue) {
+                $scope.pool.size = $scope.defaults.size;
+                $scope.crushrulesets[newValue].active_sub_rule = 0;
+                $scope.crushrulesets[oldValue].active_sub_rule = 0;
+            });
+        }
         return {
             calculatePGNum: calculatePGNum,
             validateMaxMin: validateMaxMin,
-            roundUpToNextPowerOfTwo: roundUpToNextPowerOfTwo
+            roundUpToNextPowerOfTwo: roundUpToNextPowerOfTwo,
+            getActiveRule: getActiveRule,
+            makeReset: makeReset,
+            addWatches: addWatches
         };
     });
 })();
