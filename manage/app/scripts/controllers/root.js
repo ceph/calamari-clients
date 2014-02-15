e8814051410a1451b52b966642cb5fb7de8f1359
Yan-Fa Li <yanfali@gmail.com>
2014-02-14 16:51:14 -0800

    Give the cluster membership page some attention
    - Side by side layout
    - A little ng-animate
    - font awesome icons

--- manage/app/scripts/controllers/root.js
+++ manage/app/scripts/controllers/root.js
@@ -12,35 +12,16 @@
                 $scope.minionsCounts = {
                     total: minions.length
                 };
-                $scope.minions = _.map(minions, function(minion) {
+                $scope.minions = _.zip(_.reduce(minions, function(results, minion, index) {
                     var shortName = _.first(__split.call(minion.id, '.'));
                     minion.shortName = shortName;
-                    $log.debug(shortName);
-                    $log.debug(minion);
-                    return minion;
-                });
+                    results[index % 2].push(minion);
+                    return results;
+                }, [
+                    [],
+                    []
+                ]));
             });
-            $scope.tooltip1 = {
-                title: 'Hostname: Pina01',
-                placement: 'top',
-                trigger: 'hover',
-                animation: ''
-            };
-            $scope.tooltip2 = {
-                title: 'Hostname: Pina02',
-                placement: 'top',
-                trigger: 'hover',
-            };
-            $scope.tooltip3 = {
-                title: 'Hostname: Pina03',
-                placement: 'top',
-                trigger: 'hover',
-            };
-            $scope.tooltip4 = {
-                title: 'Hostname: Pina04',
-                placement: 'top',
-                trigger: 'hover',
-            };
         };
         return ['$log', '$scope', 'KeyService', 'ClusterService', RootController];
     });
