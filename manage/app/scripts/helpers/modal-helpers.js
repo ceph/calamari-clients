6eb360269762a0fb123c157ec88c37f4ea4bedf8
Yan-Fa Li <yanfali@gmail.com>
2014-02-14 23:01:13 -0800

    Add UnexpectedError modal helper
    - update SuccessfulRequest so you can't click away as there's currently
      no way to add a custom click handler

--- manage/app/scripts/helpers/modal-helpers.js
+++ manage/app/scripts/helpers/modal-helpers.js
@@ -12,13 +12,24 @@
                     template: 'views/custom-modal.html'
                 }, options));
             },
+            UnexpectedError: function($modal, options) {
+                options = options || {
+                    status: '?'
+                };
+                return $modal(_.extend({
+                    title: 'UNEXPECTED ERROR (' + options.status + ')',
+                    container: 'body',
+                    template: 'views/custom-modal.html'
+                }, options));
+            },
             SuccessfulRequest: function($modal, options) {
                 options = options || {};
                 return $modal(_.extend({
                     content: 'This may take a few seconds. We\'ll let you know when it\'s done.',
                     container: 'body',
                     template: 'views/custom-modal.html',
-                    backdrop: 'static' // disable mouse clicks for now since I can't wrap them or supply a callback
+                    backdrop: 'static', // disable mouse clicks for now since I can't wrap them or supply a callback
+                    keyboard: false
                 }, options));
             }
         };
