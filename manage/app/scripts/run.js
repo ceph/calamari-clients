6365a8c53b0f4caa6ac6a3a466c355e346bb068b
Yan-Fa Li <yanfali@gmail.com>
2014-02-20 22:23:26 -0800

    Move request tasks list to app bar
    - invoke the scope function from the manageApp via the Request Module scope

--- manage/app/scripts/run.js
+++ manage/app/scripts/run.js
@@ -2,12 +2,16 @@
 (function() {
     'use strict';
     define(['angular'], function() {
-        /* Setup Broadcast Service With Correct Root Scopes */
         var runBlock = function($rootScope, MenuService) {
+            // set up route change handler
             $rootScope.$on('$routeChangeSuccess', function(event, to) {
                 MenuService.setActive(to.menuId);
                 $rootScope.menus = MenuService.getMenus();
             });
+            // add show requests handler for request queue
+            $rootScope.showRequests = function() {
+                angular.element(document.getElementsByClassName('RequestManagement')[0]).scope().show();
+            };
         };
         return ['$rootScope', 'MenuService', runBlock];
     });
