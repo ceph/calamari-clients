93266d1e38a6205999d316286b6001b8a96d6985
Yan-Fa Li <yanfali@gmail.com>
2014-02-26 13:46:59 -0800

    Update VM bootstrap script

--- utils/01-bootstrap-pkg.sh
+++ utils/01-bootstrap-pkg.sh
@@ -1,4 +1,5 @@
-sudo apt-get -y install vim tmux zsh ruby1.9.3
+sudo apt-get update
+sudo apt-get -y install vim tmux zsh ruby1.9.3 git curl htop
 mkdir -p ~/.tmuxinator
 cp -av /clients.git/utils/tmuxinator/session.yml ~/.tmuxinator
 wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
