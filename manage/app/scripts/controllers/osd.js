34b3ddc144fa22a62ceee3319eb72d0a72cbb499
Yan-Fa Li <yanfali@gmail.com>
2014-03-17 14:12:44 -0700

    Improve utility of OSD top level
    - show IDs of OSDs running on this host
    - make IDs clickable
    - show total OSD count for this host

--- manage/app/scripts/controllers/osd.js
+++ manage/app/scripts/controllers/osd.js
@@ -11,10 +11,34 @@
             ClusterService.get().then(function(cluster) {
                 $scope.clusterName = cluster.name;
             });
+            $scope.gotoOSD = function(id) {
+                $location.path('/osd/id/' + id);
+            };
             ServerService.getList().then(function(servers) {
                 $scope.up = true;
-                $scope.servers = _.sortBy(servers, function(server) {
-                    return server.hostname;
+                var _servers = _.reduce(servers, function(results, server) {
+                    var services = _.reduce(server.services, function(serviceResults, service) {
+                        if (service.type === 'osd') {
+                            serviceResults.osdCount += 1;
+                            serviceResults.osdID.push(service.id);
+                        }
+                        return serviceResults;
+                    }, {
+                        osdCount: 0,
+                        osdID: []
+                    });
+                    services.osdID = _.sortBy(services.osdID, function(id) {
+                        return parseInt(id, 10);
+                    });
+                    results.push({
+                        hostname: server.hostname,
+                        fqdn: server.fqdn,
+                        services: services
+                    });
+                    return results;
+                }, []);
+                $scope.servers = _.sortBy(_servers, function(server) {
+                    return server.fqdn;
                 });
             });
             $scope.hostClickHandler = function(fqdn) {
