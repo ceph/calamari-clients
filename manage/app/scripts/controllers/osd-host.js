69ba85fa985675c7e4ad04e751b50e91c1f6cec1
Yan-Fa Li <yanfali@gmail.com>
2014-03-20 22:52:27 -0700

    Enable Dropdown functionality on OSD screen
    - extract markup into common text object
    - expand parameters on click handler
    - implement placeholder UI behavior
    - build angular-strap dropdown config for each osd

--- manage/app/scripts/controllers/osd-host.js
+++ manage/app/scripts/controllers/osd-host.js
@@ -3,7 +3,18 @@
     'use strict';
     define(['lodash', 'helpers/modal-helpers'], function(_, modalHelpers) {
 
-        var OSDHostController = function($q, $log, $scope, $routeParams, ClusterService, ServerService, $location, OSDService, $modal) {
+        var text = {
+            'down': '<i class="fa fa-arrow-circle-down fa-fw fa-lg"></i>&nbsp;DOWN',
+            'in': '<i class="fa fa-sign-out fa-fw fa-lg"></i>&nbsp;IN',
+            'out': '<i class="fa fa-sign-out fa-fw fa-lg"></i>&nbsp;OUT',
+            'scrub': '<i class="fa fa-medkit fa-fw fa-lg"></i>&nbsp;SCRUB',
+            'deep_scrub': '<i class="fa fa-stethoscope fa-fw fa-lg"></i>&nbsp;DEEP SCRUB',
+            'repair': '<i class="fa fa-ambulance fa-fw fa-lg"></i>&nbsp;REPAIR',
+            'repairButton': '<i class="fa fa-medkit fa-fw fa-lg"></i>',
+            'configButton': '<i class="fa fa-gear fa-fw fa-lg"></i>',
+            'spinner': '<i class="fa fa-spinner fa-spin fa-fw fa-lg"></i>'
+        };
+        var OSDHostController = function($q, $log, $scope, $routeParams, ClusterService, ServerService, $location, OSDService, $modal, $timeout) {
             $scope.fqdn = $routeParams.fqdn;
             $scope.clusterName = ClusterService.clusterModel.name;
             $scope.modifyFn = function(id) {
@@ -29,19 +40,28 @@
                     }, modalHelpers.makeOnError(modal));
                 };
             }
-            $scope.clickHandler = function($event) {
-                $log.debug('CLICKED!', $event);
+            $scope.clickHandler = function($event, id, cmd, index) {
                 $event.preventDefault();
                 $event.stopPropagation();
+                $log.debug('CLICKED osd ' + id + ' command ' + cmd);
+                var osd = $scope.services.osds[index];
+                osd.disabled = true;
+                if (cmd === 'down' || cmd === 'out' || cmd === 'in') {
+                    osd.configText = text.spinner;
+                } else {
+                    osd.repairText = text.spinner;
+                }
+                $timeout(function() {
+                    osd.disabled = false;
+                    if (cmd === 'down' || cmd === 'out' || cmd === 'in') {
+                        osd.configText = text.configButton;
+                    } else {
+                        osd.repairText = text.repairButton;
+                    }
+                }, 1000);
                 return false;
             };
             $scope.scrubFn = makeOSDCommand('Scrub', OSDService.scrub);
-            $scope.ddconfig = [{
-                    'text': '<i class="fa fa-arrow-circle-down fa-fw fa-lg"></i>&nbsp;DOWN'
-                }, {
-                    'text': '<i class="fa fa-sign-out fa-fw fa-lg"></i>&nbsp;OUT'
-                }
-            ];
             ServerService.get($scope.fqdn).then(function(server) {
                 //console.log(server);
                 $scope.server = server;
@@ -66,28 +86,57 @@
                 $q.all(r.promises).then(function(results) {
                     _.each(results, function(result, index) {
                         /* jshint camelcase:false */
+                        result.repairText = text.repairButton;
+                        result.configText = text.configButton;
                         result.repairDropdown = _.reduce(result.valid_commands, function(newdropdown, cmd) {
                             if (cmd === 'scrub') {
                                 newdropdown.push({
-                                    'text': '<i class="fa fa-medkit fa-fw fa-lg"></i>&nbsp;SCRUB',
+                                    'text': text.scrub,
                                     'id': result.id,
-                                    'cmd': cmd
+                                    'cmd': cmd,
+                                    'index': index
                                 });
                             } else if (cmd === 'deep_scrub') {
                                 newdropdown.push({
-                                    'text': '<i class="fa fa-stethoscope fa-fw fa-lg"></i>&nbsp;DEEP SCRUB',
+                                    'text': text.deep_scrub,
                                     'id': result.id,
-                                    'cmd': cmd
+                                    'cmd': cmd,
+                                    'index': index
                                 });
                             } else if (cmd === 'repair') {
                                 newdropdown.push({
-                                    'text': '<i class="fa fa-ambulance fa-fw fa-lg"></i>&nbsp;REPAIR',
+                                    'text': text.repair,
                                     'id': result.id,
-                                    'cmd': cmd
+                                    'cmd': cmd,
+                                    'index': index
                                 });
                             }
                             return newdropdown;
                         }, []);
+                        result.configDropdown = [];
+                        if (result.up) {
+                            result.configDropdown.push({
+                                'text': text.down,
+                                'id': result.id,
+                                'cmd': 'down',
+                                'index': index
+                            });
+                        }
+                        if (result['in']) {
+                            result.configDropdown.push({
+                                'text': text.out,
+                                'id': result.id,
+                                'cmd': 'out',
+                                'index': index
+                            });
+                        } else {
+                            result.configDropdown.push({
+                                'text': text['in'],
+                                'id': result.id,
+                                'cmd': 'in',
+                                'index': index
+                            });
+                        }
                         r.osds[index] = _.extend(r.osds[index], result);
                     });
                     $scope.services = {
@@ -97,6 +146,6 @@
             });
 
         };
-        return ['$q', '$log', '$scope', '$routeParams', 'ClusterService', 'ServerService', '$location', 'OSDService', '$modal', OSDHostController];
+        return ['$q', '$log', '$scope', '$routeParams', 'ClusterService', 'ServerService', '$location', 'OSDService', '$modal', '$timeout', OSDHostController];
     });
 })();
