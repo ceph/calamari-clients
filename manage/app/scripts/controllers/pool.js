8249480e934426a3c1050ff8593791d5bfaa0be0
Yan-Fa Li <yanfali@gmail.com>
2014-03-28 10:28:52 -0700

    Enable Delete from Pool Summary
    - port delete flow from pool-modify
    - add a callback to re-scan the pool list after successful delete

--- manage/app/scripts/controllers/pool.js
+++ manage/app/scripts/controllers/pool.js
@@ -1,9 +1,9 @@
 /* global define */
 (function() {
     'use strict';
-    define([], function() {
+    define(['helpers/modal-helpers'], function(modalHelpers) {
 
-        var PoolController = function($log, $scope, PoolService, ClusterService, $location) {
+        var PoolController = function($log, $scope, PoolService, ClusterService, $location, $modal, RequestTrackingService) {
             if (ClusterService.clusterId === null) {
                 $location.path('/first');
                 return;
@@ -29,7 +29,67 @@
             $scope.modify = function(id) {
                 $location.path('/pool/modify/' + id);
             };
+            $scope.remove = function(pool) {
+                $log.debug('deleting ' + pool.id);
+                var modal = $modal({
+                    title: 'This will DELETE the \'' + pool.name + '\' Pool. Are you sure?',
+                    content: 'There is no way to undo this operation. Please be sure this is what you are trying to do.',
+                    template: 'views/delete-modal.html'
+                });
+                modal.$scope.id = pool.id;
+                modal.$scope.cancel = function() {
+                    modal.$scope.$hide();
+                };
+                modal.$scope.confirm = function() {
+                    modal.$scope.$hide();
+                    PoolService.remove(modal.$scope.id).then(function(result) {
+                        if (result.status === 202) {
+                            /* jshint camelcase: false */
+                            RequestTrackingService.add(result.data.request_id, function() {
+                                PoolService.getList().then(function(pools) {
+                                    $scope.pools = pools;
+                                });
+                            });
+                            var okmodal = modalHelpers.SuccessfulRequest($modal, {
+                                title: 'Delete Request Successful'
+                            });
+                            okmodal.$scope._hide = function() {
+                                okmodal.$scope.$hide();
+                                $location.path('/pool');
+                            };
+                            return;
+                        }
+                        var umodal = modalHelpers.SuccessfulRequest($modal, {
+                            title: 'Delete Pool Request Completed',
+                            content: result.data
+                        });
+                        umodal.$scope._hide = function() {
+                            umodal.$scope.$hide();
+                            $location.path('/pool');
+                        };
+                    }, function(error) {
+                        $log.error(error);
+                        var errModal;
+                        if (error.status === 403) {
+                            errModal = modalHelpers.UnAuthorized($modal, {});
+                            errModal.$scope._hide = function() {
+                                errModal.$scope.$hide();
+                                $location.path('/pool');
+                            };
+                            return;
+                        }
+                        errModal = modalHelpers.UnexpectedError($modal, {
+                            status: error.status,
+                            content: error.data
+                        });
+                        errModal.$scope._hide = function() {
+                            modal.$scope.$hide();
+                            $location.path('/pool');
+                        };
+                    });
+                };
+            };
         };
-        return ['$log', '$scope', 'PoolService', 'ClusterService', '$location', PoolController];
+        return ['$log', '$scope', 'PoolService', 'ClusterService', '$location', '$modal', 'RequestTrackingService', PoolController];
     });
 })();
