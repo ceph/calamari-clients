eb5c65cb7f3962098d27eec8648b03f8fa8bfaaf
Yan-Fa Li <yanfali@gmail.com>
2014-04-04 00:17:10 -0700

    Tracker WIP

---
+++ dashboard/app/scripts/collections/user-request-collection.js
@@ -0,0 +1,20 @@
+/*jshint -W106*/
+/*global define*/
+
+define(['underscore', 'backbone', 'models/user-request-model'], function(_, Backbone, UserRequestModel) {
+    'use strict';
+
+    var UserRequestCollection = Backbone.Collection.extend({
+        url: function() {
+            return '/api/v2/cluster/' + this.cluster + '/request';
+        },
+        model: UserRequestModel,
+        initialize: function(models, options) {
+            if (options && options.cluster) {
+                this.cluster = options.cluster;
+            }
+        },
+    });
+
+    return UserRequestCollection;
+});
---
+++ dashboard/app/scripts/tracker.js
@@ -0,0 +1,33 @@
+/* global define */
+
+define(['jquery', 'underscore', 'backbone', 'idbwrapper', 'loglevel', 'collections/user-request-collection', 'models/user-request-model', 'marionette'], function($, _, Backbone, IDBStore, log, UserRequestCollection, UserRequestModel) {
+    'use strict';
+
+    return Backbone.Marionette.ItemView.extend({
+        initialize: function() {
+            this.App = Backbone.Marionette.getOption(this, 'App');
+            this.cluster = Backbone.Marionette.getOption(this, 'cluster');
+            this.collection = new UserRequestCollection({
+                cluster: this.cluster
+            });
+            this.model = new UserRequestModel({
+                cluster: this.cluster
+            });
+            this.listenTo(this.App.vent, 'cluster:update', this.updateFSID);
+            this.requests = new IDBStore({
+                dbVersion: 2,
+                storeName: 'InktankUserRequest',
+                keyPath: 'id',
+                autoIncrement: false,
+                onStoreReady: function() {
+                    log.info('Inktank User Request Store ready!');
+                }
+            });
+            _.bindAll(this, 'updateFSID');
+        },
+        updateFSID: function(cluster) {
+            this.collection.cluster = cluster.get('id');
+            this.model.set('cluster', cluster.get('id'));
+        }
+    });
+});
