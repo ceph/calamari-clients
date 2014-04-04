23e1f61e01d8f6d8fb3760bcd01bc50b17cdeaf7
Yan-Fa Li <yanfali@gmail.com>
2014-04-04 15:40:07 -0700

    Set page_size and state on collection requests

--- dashboard/app/scripts/collections/user-request-collection.js
+++ dashboard/app/scripts/collections/user-request-collection.js
@@ -6,13 +6,25 @@ define(['underscore', 'backbone', 'models/user-request-model'], function(_, Back
 
     var UserRequestCollection = Backbone.Collection.extend({
         url: function() {
-            return '/api/v2/cluster/' + this.cluster + '/request';
+            return '/api/v2/cluster/' + this.cluster + '/request' + this.params;
+        },
+        parse: function(resp) {
+            return resp.results;
         },
         model: UserRequestModel,
+        params: '?page_size=32',
         initialize: function(models, options) {
             if (options && options.cluster) {
                 this.cluster = options.cluster;
             }
+        },
+        // Only grab requests with state submitted
+        getSubmitted: function(options) {
+            this.params = '?state=submitted&page_size=32';
+            var self = this;
+            return this.fetch(options).always(function() {
+                self.params = '?page_size=32';
+            });
         }
     });
 
