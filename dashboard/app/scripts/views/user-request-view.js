00346361b1eb54ab80c24e7ab0e226ed1586c97d
Yan-Fa Li <yanfali@gmail.com>
2014-04-10 11:52:22 -0700

    Restore fade-and-slide animation to requests
    - implement just enough behavior to trigger the angular-animation style
      of css3 animations.
    - clean up initialization of user-request view

--- dashboard/app/scripts/views/user-request-view.js
+++ dashboard/app/scripts/views/user-request-view.js
@@ -1,10 +1,10 @@
 /* global define */
-define(['jquery', 'underscore', 'templates', 'backbone', 'collections/user-request-collection', 'l20nCtx!locales/{{locale}}/strings', 'moment', 'marionette'], function($, _, JST, Backbone, UserRequestCollection, l10n, moment) {
+define(['jquery', 'underscore', 'templates', 'backbone', 'collections/user-request-collection', 'l20nCtx!locales/{{locale}}/strings', 'moment', 'helpers/animation', 'marionette'], function($, _, JST, Backbone, UserRequestCollection, l10n, moment) {
     'use strict';
     var UserRequestView = Backbone.Marionette.ItemView.extend({
         template: JST['app/scripts/templates/user-request.ejs'],
-        request: JST['app/scripts/templates/request.ejs'],
-        norequest: JST['app/scripts/templates/no-request.ejs'],
+        requestTemplate: JST['app/scripts/templates/request.ejs'],
+        norequestTemplate: JST['app/scripts/templates/no-request.ejs'],
         tagName: 'div',
         className: '',
         events: {
@@ -14,13 +14,20 @@ define(['jquery', 'underscore', 'templates', 'backbone', 'collections/user-reque
             'tbody': 'tbody'
         },
         initialize: function() {
+            _.bindAll(this, 'show', 'hide', 'updateCollection', 'buildCollectionView');
             this.App = Backbone.Marionette.getOption(this, 'App');
             this.clusterId = Backbone.Marionette.getOption(this, 'cluster');
             this.collection = new UserRequestCollection([], {
                 cluster: this.clusterId
             });
-            this.listenTo(this.App.vent, 'UserRequestView:toggle', this.show);
-            _.bindAll(this, 'show', 'hide');
+            this.listenToOnce(this, 'render', function() {
+                // animate first render
+                this.collection.fetch().then(this.updateCollection);
+                var $el = this.$('.am-fade-and-slide-right').addClass('ng-enter');
+                setTimeout(function() {
+                    $el.addClass('ng-enter-active');
+                }.bind(this), 10);
+            }.bind(this));
         },
         serializeData: function() {
             return {
@@ -31,42 +38,53 @@ define(['jquery', 'underscore', 'templates', 'backbone', 'collections/user-reque
                 closeBtn: l10n.getSync('UserRequestViewCloseBtn')
             };
         },
+        buildCollectionView: function(results) {
+            return _.reduce(results, function(result, task) {
+                var clazz;
+                switch (task.state) {
+                    case 'submitted':
+                        clazz = 'text-info fa fa-spinner fa-spin';
+                        break;
+                    case 'error':
+                        clazz = 'text-warning fa fa-exclamation-circle';
+                        break;
+                    default:
+                        clazz = 'text-success fa fa-check-circle';
+                        break;
+                }
+                /* jshint camelcase: false */
+                var time = moment(task.state === 'complete' ? task.completed_at : task.requested_at).fromNow();
+                result.push(this.requestTemplate({
+                    clazz: clazz,
+                    headline: task.headline,
+                    state: task.state,
+                    time: time
+                }));
+                return result;
+            }.bind(this), []);
+        },
+        updateCollection: function(resp) {
+            if (resp.results.length === 0) {
+                this.ui.tbody.html(this.norequestTemplate());
+                return;
+            }
+            this.ui.tbody.html(this.buildCollectionView(resp.results).join(''));
+        },
         show: function() {
             this.$el.show();
-            var self = this;
-            this.collection.fetch().then(function(resp) {
-                if (resp.results.length === 0) {
-                    self.ui.tbody.html(self.norequest());
-                } else {
-                    var markup = _.reduce(resp.results, function(result, task) {
-                        var clazz;
-                        switch (task.state) {
-                            case 'submitted':
-                                clazz = 'text-info fa fa-spinner fa-spin';
-                                break;
-                            case 'error':
-                                clazz = 'text-warning fa fa-exclamation-circle';
-                                break;
-                            default:
-                                clazz = 'text-success fa fa-check-circle';
-                                break;
-                        }
-                        /* jshint camelcase: false */
-                        var time = moment(task.state === 'complete' ? task.completed_at : task.requested_at).fromNow();
-                        result.push(self.request({
-                            clazz: clazz,
-                            headline: task.headline,
-                            state: task.state,
-                            time: time
-                        }));
-                        return result;
-                    }, []);
-                    self.ui.tbody.html(markup.join(''));
-                }
-            });
+            this.$('.am-fade-and-slide-right').addClass('ng-enter');
+            setTimeout(function() {
+                this.$('.ng-enter').addClass('ng-enter-active');
+                this.collection.fetch().then(this.updateCollection);
+            }.bind(this), 10);
         },
         hide: function() {
-            this.$el.hide();
+            var $el = this.$('.am-fade-and-slide-right');
+            $el.removeClass('ng-enter ng-enter-active').addClass('ng-leave');
+            setTimeout(function() {
+                this.$el.hide();
+                $el.removeClass('ng-leave');
+            }.bind(this), 300);
         }
     });
     return UserRequestView;
