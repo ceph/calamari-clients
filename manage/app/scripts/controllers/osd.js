0c5acb910e5fa603a551c07ea3e6bb4fb8dea694
Yan-Fa Li <yanfali@gmail.com>
2014-04-17 12:11:33 -0700

    Make the OSD List less Janky
    - wait up to 500ms after the request for individual status has completed
    - fade in the host entries
    - restrict the layout of the table headers to avoid re-layout jank

--- manage/app/scripts/controllers/osd.js
+++ manage/app/scripts/controllers/osd.js
@@ -3,7 +3,7 @@
     'use strict';
     define(['lodash', 'helpers/osd-helpers'], function(_, osdHelpers) {
 
-        var OSDController = function($scope, ClusterService, ServerService, $location, OSDService, $modal, PoolService) {
+        var OSDController = function($scope, ClusterService, ServerService, $location, OSDService, $modal, PoolService, $timeout) {
             if (ClusterService.clusterId === null) {
                 $location.path('/first');
                 return;
@@ -27,53 +27,58 @@
                     });
                 });
             };
+            var start = Date.now();
             ServerService.getList().then(function(servers) {
-                $scope.up = true;
-                var _servers = _.reduce(servers, function(results, server) {
-                    var services = _.reduce(server.services, function(serviceResults, service) {
-                        if (service.type === 'osd') {
-                            serviceResults.osdCount += 1;
-                            serviceResults.osdID.push(service.id);
-                        }
-                        return serviceResults;
-                    }, {
-                        osdCount: 0,
-                        osdID: []
-                    });
-                    services.osdID = _.sortBy(services.osdID, function(id) {
-                        return parseInt(id, 10);
-                    });
-                    services.state = _.reduce(services.osdID, function(result) {
-                        result.push(2);
-                        return result;
-                    }, []);
-                    OSDService.getSet(services.osdID).then(function(osds) {
-                        services.state = _.reduce(osds, function(result, osd) {
-                            var state = 2;
-                            if (!osd.up && !osd['in']) {
-                                state = 0;
-                            } else if (!osd.up || !osd['in']) {
-                                state = 1;
+                var elapsed = Date.now() - start;
+                var timeout = elapsed < 500 ? 500 - elapsed : 0;
+                $timeout(function() {
+                    var _servers = _.reduce(servers, function(results, server) {
+                        var services = _.reduce(server.services, function(serviceResults, service) {
+                            if (service.type === 'osd') {
+                                serviceResults.osdCount += 1;
+                                serviceResults.osdID.push(service.id);
                             }
-                            result.push(state);
+                            return serviceResults;
+                        }, {
+                            osdCount: 0,
+                            osdID: []
+                        });
+                        services.osdID = _.sortBy(services.osdID, function(id) {
+                            return parseInt(id, 10);
+                        });
+                        services.state = _.reduce(services.osdID, function(result) {
+                            result.push(2);
                             return result;
                         }, []);
+                        OSDService.getSet(services.osdID).then(function(osds) {
+                            services.state = _.reduce(osds, function(result, osd) {
+                                var state = 2;
+                                if (!osd.up && !osd['in']) {
+                                    state = 0;
+                                } else if (!osd.up || !osd['in']) {
+                                    state = 1;
+                                }
+                                result.push(state);
+                                return result;
+                            }, []);
+                        });
+                        results.push({
+                            hostname: server.hostname,
+                            fqdn: server.fqdn,
+                            services: services
+                        });
+                        return results;
+                    }, []);
+                    $scope.servers = _.sortBy(_servers, function(server) {
+                        return server.fqdn;
                     });
-                    results.push({
-                        hostname: server.hostname,
-                        fqdn: server.fqdn,
-                        services: services
-                    });
-                    return results;
-                }, []);
-                $scope.servers = _.sortBy(_servers, function(server) {
-                    return server.fqdn;
-                });
+                }, timeout);
+                $scope.up = true;
             });
             $scope.hostClickHandler = function(fqdn) {
                 $location.path('/osd/server/' + fqdn);
             };
         };
-        return ['$scope', 'ClusterService', 'ServerService', '$location', 'OSDService', '$modal', 'PoolService', OSDController];
+        return ['$scope', 'ClusterService', 'ServerService', '$location', 'OSDService', '$modal', 'PoolService', '$timeout', OSDController];
     });
 })();
