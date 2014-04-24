d3f3d464f57e4437b4eec57759dd59849ba88a17
Yan-Fa Li <yanfali@gmail.com>
2014-04-23 22:23:52 -0700

    Apply a once function to modal
    You only want this to run one time. Subsequent 403 errors can be
    ignored.

--- manage/app/scripts/services/error.js
+++ manage/app/scripts/services/error.js
@@ -1,26 +1,29 @@
 /*global define*/
-define(['lodash'], function() {
+define(['lodash'], function(_) {
     'use strict';
     var ErrorService = function($log, $modal) {
+        var once = _.once(function unauthorized() {
+            var modal = $modal({
+                title: '<i class="text-danger fa fa-exclamation-circle"></i> Unauthorized Access',
+                content: 'Your login appears to have expired. Try logging back in again.',
+                show: true,
+                html: true,
+                backdrop: 'static'
+            });
+            modal.$scope.$hide = function() {
+                document.location = '/login/';
+            };
+        });
         var Service = {
             errorInterceptor: function(response) {
                 if (response.status === 403) {
-                    var modal = $modal({
-                        title: '<i class="text-danger fa fa-exclamation-circle"></i> Unauthorized Access',
-                        content: 'Your login appears to have expired. Try logging back in again.',
-                        show: true,
-                        html: true,
-                        backdrop: 'static'
-                    });
-                    modal.$scope.$hide = function() {
-                        document.location = '/login/';
-                    };
-                return false;
+                    once();
+                    return false;
+                }
+                return response;
             }
-            return response;
-        }
+        };
+        return Service;
     };
-    return Service;
-};
-return ['$log', '$modal', '$location', ErrorService];
+    return ['$log', '$modal', '$location', ErrorService];
 });
