077d0899e799c876041ebba2bd402888e6bc1d58
Yan-Fa Li <yanfali@gmail.com>
2014-04-30 16:46:41 -0700

    Issue #8262 Dashboard About Modal
    port work from manage over to dashboard dropdown

--- dashboard/app/scripts/views/userdropdown-view.js
+++ dashboard/app/scripts/views/userdropdown-view.js
@@ -1,5 +1,5 @@
 /* global define */
-define(['jquery', 'underscore', 'templates', 'backbone', 'marionette'], function($, _, JST, Backbone) {
+define(['jquery', 'underscore', 'templates', 'backbone', 'Backbone.Modal', 'marionette'], function($, _, JST, Backbone) {
     'use strict';
     var UserDropDown = Backbone.Marionette.ItemView.extend({
         template: JST['app/scripts/templates/userdropdown.ejs'],
@@ -7,7 +7,8 @@ define(['jquery', 'underscore', 'templates', 'backbone', 'marionette'], function
         className: 'nav navbar-nav navbar-right',
         events: {
             'click .logout': 'logout',
-            'click .settings': 'settings'
+            'click .settings': 'settings',
+            'click .about': 'about'
         },
         initialize: function() {
             this.App = Backbone.Marionette.getOption(this, 'App');
@@ -20,7 +21,9 @@ define(['jquery', 'underscore', 'templates', 'backbone', 'marionette'], function
                 settingsIcon: 'fa fa-cogs',
                 settingsLabel: 'Settings',
                 logoutIcon: 'fa fa-power-off',
-                logoutLabel: 'Logout'
+                logoutLabel: 'Logout',
+                aboutIcon: 'fa fa-info-circle',
+                aboutLabel: 'About Calamari'
             });
             this.listenTo(this.model, 'change', this.render);
             _.bindAll(this, 'settings', 'logout');
@@ -29,15 +32,30 @@ define(['jquery', 'underscore', 'templates', 'backbone', 'marionette'], function
             return this.model.fetch();
         },
         logout: function() {
-            // TODO look at history API
             var d = $.get('/api/v1/auth/logout');
             d.always(function() {
                 document.location = '/login/';
             });
         },
         settings: function() {
-            // TODO look at history API
             document.location = '/admin/';
+        },
+        about: function() {
+            $.get('/api/v2/info').then(function(resp) {
+                var Modal = Backbone.Modal.extend({
+                    template: function() {
+                        return JST['app/scripts/templates/about-modal.ejs']({
+                            version: {
+                                calamariAPI: resp.version,
+                                client: window.inktank.commit
+                            }
+                        });
+                    },
+                    cancelEl: '.btn-primary'
+                });
+                var modal = new Modal();
+                $('body').append(modal.render().el);
+            });
         }
 
     });
