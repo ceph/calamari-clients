ed24a692ba7d0252d26ced63d6d5be7ed7b106a8
Yan-Fa Li <yanfali@gmail.com>
2014-05-12 15:49:11 -0700

    Annotation app.js

--- dashboard/app/scripts/app.js
+++ dashboard/app/scripts/app.js
@@ -17,6 +17,7 @@ require(['jquery', 'underscore', 'backbone', 'loglevel', 'humanize', 'views/appl
             initial = 'graphmode';
             anchor = 'graph/all';
         }
+        // Once we have processed the target param, delete it from the URL
         uri.deleteQueryParam('target');
         uri.setAnchor(anchor);
         history.pushState('', 'Dashboard', uri.toString());
@@ -45,7 +46,7 @@ require(['jquery', 'underscore', 'backbone', 'loglevel', 'humanize', 'views/appl
         'enable-demo-mode': false
     };
 
-    /* Default Configuration */
+    /* Default Backbone Router Configuration */
     var AppRouter = Backbone.Router.extend({
         routes: {
             'workbench': 'workbench',
@@ -54,15 +55,17 @@ require(['jquery', 'underscore', 'backbone', 'loglevel', 'humanize', 'views/appl
         }
     });
     var appRouter = new AppRouter();
+
     /* Load Config.json first before starting app */
     var configUrl = 'scripts/config.json';
+    // Load config.json using a promise. This is the error handler.
     var promise = configloader(configUrl).then(undefined, function(jqXHR, textStatus, errorThrown) {
         if (_.isString(jqXHR)) {
+            // Error was probably caused a JSON parsing error. Pass through the error string.
             console.log(jqXHR);
-            // Create a Wait Function which we pass through to promise callbacks.
-            // The last callback is responsible for invoking it, in this
-            // case after the AlertView has been set up. This is a deferred event,
-            // otherwise there would be nothing to receive it.
+            // In order to get a pretty error message, we have to complete loading of widgets, including AlertView.
+            // This creates a callback which gets executed after the normal app initialization has completed
+            // allowing the UI to report the error rather than stopping the UI.
             var waitAfterLoadedFn = function() {
                 this.inktank.App.vent.trigger('app:configerror', jqXHR);
             };
@@ -70,46 +73,63 @@ require(['jquery', 'underscore', 'backbone', 'loglevel', 'humanize', 'views/appl
         } else if (_.isObject(jqXHR) && jqXHR.readState) {
             console.log(errorThrown + ' loading ' + configUrl);
         } else {
+            // No config.json was found. Treat this is a non-error by returning a resovled promise.
             console.log('No ' + configUrl + ' found. Using app defaults');
             return $.Deferred().resolve();
         }
     });
+
+    // One we've loaded config.json successfully configure the graphite-host and iops-host
+    // values which are used by Graph to make requests for metrics.
     promise.done(function(result) {
         _.extend(config, result);
         if (config['graphite-host'] && config['iops-host'] === undefined) {
             config['iops-host'] = config['graphite-host'];
         }
     });
-    /* Load Config.json first before starting app */
 
+    // ###Dashboard initialization starts here
     var App, userMenu, clusterMenu;
     promise.then(function(waitFn) {
+
+        // Create the Application Object
         App = new Application();
+        // TODO This instance should probably go away as Application comes with one.
         App.ReqRes = new Backbone.Wreqr.RequestResponse();
+        // Attach our config object to app so all components have access to it.
         App.Config = config;
+
+        
         userMenu = new UserDropDown({
             el: $('.usermenu'),
             App: App
         });
+        // User dropdown menu needs user meta-data. It's not critical path so load it in the background.
         userMenu.fetch();
 
-        /* Widget Setup */
+        // Top Row Dashboard - aka gauges. Uses Marionette Layout to contain widgets.
         var gaugesLayout = new views.GaugesLayout({
             el: '.gauges'
         });
         gaugesLayout.render();
+
+        // All Components get an instance of App so they get access to the event bus,
+        // request response and config.
         var healthView = new views.HealthView({
             App: App,
             model: new models.HealthModel()
         });
+
         gaugesLayout.a.show(healthView);
         var osdView = new views.OsdView({
             App: App
         });
+
         gaugesLayout.b.show(osdView);
         var monView = new views.MonView({
             App: App
         });
+
         gaugesLayout.c.show(monView);
         var gauge = new views.UsageView({
             App: App,
@@ -117,6 +137,13 @@ require(['jquery', 'underscore', 'backbone', 'loglevel', 'humanize', 'views/appl
             title: 'Usage'
         });
 
+        var poolsView = new views.PoolsView({
+            App: App
+        });
+        gaugesLayout.d.show(poolsView);
+
+ 
+        // Middle Row of Dashboard
         var mapsLayout = new views.GaugesLayout({
             el: '.maps'
         });
@@ -128,11 +155,7 @@ require(['jquery', 'underscore', 'backbone', 'loglevel', 'humanize', 'views/appl
         mapsLayout.a.show(pgView);
 
 
-        var poolsView = new views.PoolsView({
-            App: App
-        });
-        gaugesLayout.d.show(poolsView);
-
+        // Bottom row of Dashboard Layout Widget
         var iopsLayout = new views.GaugesLayout({
             el: '.iops'
         });
@@ -143,20 +166,26 @@ require(['jquery', 'underscore', 'backbone', 'loglevel', 'humanize', 'views/appl
         });
         iopsLayout.c.show(hostsView);
 
+        // OSD Collection instance is initialized here. This is the vestiges of our former offline and demo modes.
         var collection;
         collection = config.offline ? Generate.osds(160) : new Collection([], {});
+
+        // Start the OSD Visualization. It isn't rendered yet, but it's needed for other components to query against.
         var viz = new views.OSDVisualization({
             App: App,
             collection: collection,
             el: '.raphael-one'
         });
 
+        // Deprecated - key handlers are installed
         if (config['enable-demo-mode']) {
             $('body').on('keyup', function(evt) {
                 App.vent.trigger('keyup', evt);
             });
         }
 
+        // Reconfigure humanize.js with labels we like better.
+        // TODO add this text to l20n.js
         _.extend(humanize.catalog, {
             'seconds_ago': ' secs ago',
             'about_a_minute_ago': '1 min ago',
@@ -167,9 +196,9 @@ require(['jquery', 'underscore', 'backbone', 'loglevel', 'humanize', 'views/appl
             'days_ago': 'd ago'
         });
 
-        /* Widget Setup */
-
-        /* Defer Visualization startup to after loading the cluster metadata */
+        // Defer Visualization startup to after loading the cluster metadata
+        // Now our base widgets are setup, we need to get the first cluster returned
+        // by the calamari API and use it's metadata to initialize our polling task.
         var clusterDeferred = $.Deferred();
         clusterMenu = new ClusterDropDown({
             el: $('.clustermenu'),
@@ -179,19 +208,28 @@ require(['jquery', 'underscore', 'backbone', 'loglevel', 'humanize', 'views/appl
             clusterDeferred.resolve(clusterMenu.collection.first());
         });
         clusterDeferred.promise().done(function(cluster) {
+            // **All the following components need to know what cluster they are operating against.**
+
+            // Now we know what cluster we're operating on, load the iops view.
             var iopsView = new views.IopsView({
                 'graphiteHost': config['iops-host'],
                 App: App
             });
             iopsLayout.a.show(iopsView);
+
+            // Start the alerts view, which is responsible for all notifications in the dashboard.
             var alertsView = new views.AlertsView({
                 App: App
             });
 
+
+            // init the poller task. Starting happens after the viz renders.
             var poller = new Poller({
                 App: App,
                 cluster: cluster.get('id')
             });
+
+            // Initialize the graph wall sub-module.
             App.graphWall = new GraphWall({
                 App: App,
                 AppRouter: appRouter,
@@ -199,6 +237,7 @@ require(['jquery', 'underscore', 'backbone', 'loglevel', 'humanize', 'views/appl
                 graphiteRequestDelayMs: config['graphite-request-delay-ms']
             });
 
+            // Now we have a cluster, start the poller task so we get updates.
             viz.render().then(function() {
                 iopsLayout.b.show(gauge);
                 if (!config.offline) {
@@ -206,6 +245,7 @@ require(['jquery', 'underscore', 'backbone', 'loglevel', 'humanize', 'views/appl
                 }
             });
 
+            // Add hooks to the State Machine so it responds to routing change events.
             appRouter.on('route:workbench', function() {
                 App.fsm.viz();
             });
@@ -217,6 +257,7 @@ require(['jquery', 'underscore', 'backbone', 'loglevel', 'humanize', 'views/appl
                 App.fsm.graph(host, osd);
             });
 
+            // If a wait function was supplied invoke it now
             App.addInitializer(function() {
                 if (_.isFunction(waitFn)) {
                     // run this callback after the app has been set up
@@ -224,7 +265,7 @@ require(['jquery', 'underscore', 'backbone', 'loglevel', 'humanize', 'views/appl
                 }
             });
 
-
+            // Initialize the navigating menus at top of screen.
             var breadcrumbView = new views.BreadCrumbView({
                 App: App,
                 AppRouter: appRouter,
@@ -233,22 +274,27 @@ require(['jquery', 'underscore', 'backbone', 'loglevel', 'humanize', 'views/appl
             });
             breadcrumbView.render();
 
+            // Start the app fully
             App.start({
                 appRouter: appRouter,
                 initial: initial
             });
 
+            // Create a Request Tracker Singleton
             var userRequestTracker = new UserRequestTracker({
                 App: App,
                 cluster: cluster.get('id')
             });
 
+            // Add a View controller
             var userRequestView = new views.UserRequestView({
                 App: App,
                 cluster: cluster.get('id'),
                 el: '.userrequests'
             });
 
+            // Make the first toggle request render the User Request View and
+            // all subsequent ones hide and show the widget. Lazy init.
             App.listenToOnce(App.vent, 'UserRequestView:toggle', function() {
                 userRequestView.render();
                 App.listenTo(App.vent, 'UserRequestView:toggle', function() {
@@ -257,12 +303,13 @@ require(['jquery', 'underscore', 'backbone', 'loglevel', 'humanize', 'views/appl
             });
 
 
+            // Initialize the notification bell which shows and hides the UserRequestView
             var notificationBellView = new views.NotificationBellView({
                 App: App,
                 el: '.bell-button'
             });
 
-            // Global Exports
+            // Global Exports for Debugging
             window.inktank = {
                 App: App,
                 Router: appRouter,
