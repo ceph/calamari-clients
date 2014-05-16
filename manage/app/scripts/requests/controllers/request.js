e146b67661d79d9c90929661ba381815c280a436
Yan-Fa Li <yanfali@gmail.com>
2014-05-16 16:45:04 -0700

    Issue #8351 Update Manage Request Tracking View
    Part 2 of 2
    
    - implement an angularjs version of the polling update.
      slightly different logic as it's not clear when something is going
      run and timeouts use promises.
    - fixed a bug in the way I was handling $hide which was more obvious
      because of clean up in the handler.
    
    Signed-off-by: Yan-Fa Li <yanfali@gmail.com>

--- manage/app/scripts/requests/controllers/request.js
+++ manage/app/scripts/requests/controllers/request.js
@@ -3,7 +3,16 @@
     'use strict';
     define(['lodash', 'moment'], function(_, moment) {
 
-        var RequestController = function($log, $scope, ClusterResolver, RequestService, RequestTrackingService, $aside) {
+        var RequestController = function($timeout, $log, $scope, ClusterResolver, RequestService, RequestTrackingService, $aside) {
+            function getPollTimeFn() {
+                var delayMs = 1250;
+                return function() {
+                    if (delayMs < 20000) {
+                        delayMs *= 2;
+                    }
+                    return delayMs;
+                };
+            }
             ClusterResolver.then(function() {
                 var myAside = $aside({
                     'title': 'Requested Tasks',
@@ -13,35 +22,53 @@
                     'show': false,
                     'container': '.RequestManagement'
                 });
+
+                function responseParser(response) {
+                    response = _.map(response, function(request) {
+                        /* jshint camelcase: false */
+                        var time = request.state === 'complete' ? request.completed_at : request.requested_at;
+                        var headline = request.headline;
+                        var state = request.state;
+                        if (request.error) {
+                            state = 'error';
+                        }
+                        return {
+                            headline: headline,
+                            state: state,
+                            reltime: moment(time).fromNow(),
+                            time: moment(time).format(),
+                            error_message: request.error_message
+                        };
+                    });
+                    myAside.$scope.tasks = response;
+                    myAside.$scope.empty = response.length === 0;
+                }
+
+                $scope.timeout = false;
+
+                function refresh() {
+                    // re-install the timer even if it errors out
+                    if ($scope.timeout === false) {
+                        return;
+                    }
+                    RequestService.getList().then(responseParser)['finally'](function() {
+                        $timeout(refresh, $scope.pollTimeoutInMs());
+                    });
+                }
+
                 myAside.$scope.empty = true;
                 $scope.show = function() {
                     myAside.$scope.$show();
-                    RequestService.getList().then(function(response) {
-                        response = _.map(response, function(request) {
-                            /* jshint camelcase: false */
-                            var time = request.state === 'complete' ? request.completed_at : request.requested_at;
-                            var headline = request.headline;
-                            var state = request.state;
-                            if (request.error) {
-                                state = 'error';
-                            }
-                            return {
-                                headline: headline,
-                                state: state,
-                                reltime: moment(time).fromNow(),
-                                time: moment(time).format(),
-                                error_message: request.error_message
-                            };
-                        });
-                        myAside.$scope.tasks = response;
-                        myAside.$scope.empty = response.length === 0;
-                        myAside.$scope._hide = function() {
-                            myAside.$scope.$hide();
-                        };
+                    myAside.$scope.$hide = _.wrap(myAside.$scope.$hide, function($hide) {
+                        $hide();
+                        $scope.timeout = false;
                     });
+                    $scope.pollTimeoutInMs = getPollTimeFn();
+                    $scope.timeout = true;
+                    refresh();
                 };
             });
         };
-        return ['$log', '$scope', 'ClusterResolver', 'RequestService', 'RequestTrackingService', '$aside', RequestController];
+        return ['$timeout', '$log', '$scope', 'ClusterResolver', 'RequestService', 'RequestTrackingService', '$aside', RequestController];
     });
 })();
