619f2600321f4c789bbc29571bcc6069fecb36e2
Yan-Fa Li <yanfali@gmail.com>
2014-05-27 17:01:19 -0700

    Add docco for alerts-view
    - Remove unused code
    
    Signed-off-by: Yan-Fa Li <yanfali@gmail.com>

--- dashboard/app/scripts/views/alerts-view.js
+++ dashboard/app/scripts/views/alerts-view.js
@@ -3,16 +3,30 @@
 define(['jquery', 'underscore', 'backbone', 'templates', 'l20nCtx!locales/{{locale}}/strings', 'marionette'], function($, _, Backbone, JST, l10n) {
     'use strict';
 
+    // Respond to Event Aggregator Bus events.
+    //
+    // | Event | Meaning |
+    // |-------|---------|
+    // | app:neterror | Display information about various network errors by Status Code |
+    // | app:configerror | Usually caused by config.json syntax errors |
+    // | krakenHeartBeat:update | Backend data appears to be old, based on last updated timestamps |
+    // | request:success | Notification of successful User Request |
+    // | request:error | Notification of Failed User Request |
+    //
+    //
+    // This object handles general notifications using noty and
+    // simulates angular-growl using a specialized theme file.
+    //
     var AlertsView = Backbone.Marionette.ItemView.extend({
         template: JST['app/scripts/templates/alerts.ejs'],
         growlTemplate: JST['app/scripts/templates/growl.ejs'],
         throttleMs: 10000,
         throttleCount: 3,
         krakenFailThreshold: 1000 * 60 * 15,
+        timeoutCount: 0,
         initialize: function() {
             this.App = Backbone.Marionette.getOption(this, 'App');
             this.listenTo(this.App.vent, 'app:neterror', this.neterrorHandler);
-            this.listenTo(this.App.vent, 'app:configerror', this.configError);
             this.listenTo(this.App.vent, 'krakenHeartBeat:update', this.heartBeat);
             this.listenTo(this.App.vent, 'request:success', this.requestSuccess);
             this.listenTo(this.App.vent, 'request:error', this.requestError);
@@ -21,11 +35,14 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'l20nCtx!locales/{{loca
             }, this);
             this.sessionExpired = _.once(this.sessionExpired);
             this.serverUnreachable = _.once(this.serverUnreachable);
-            this.configError = _.once(this.configError);
             this.timeout = _.after(this.throttleCount, this.timeout);
             this.clusterAPITimeout = _.throttle(this.clusterAPITimeout, this.krakenFailThreshold);
-            _.bindAll(this, 'neterrorHandler', 'heartBeat', 'requestSuccess', 'requestError', 'configError');
+            _.bindAll(this, 'neterrorHandler', 'heartBeat', 'requestSuccess', 'requestError');
         },
+        // **heartBeat**
+        // Kraken HeartBeat Check. Compare the last cluster update time stamp against the current
+        // time and report an error if they have diverged more than 15 minutes. This can
+        // indicate loss of communication.
         heartBeat: function(model) {
             if (model) {
                 var attrs = model.attributes;
@@ -44,6 +61,8 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'l20nCtx!locales/{{loca
                 }
             }
         },
+        // **commonNotyNotification**
+        // Default values for the AngularGrowl like Noty Notifications.
         commonNotyNotification: {
             layout: 'topRight',
             template: _.template('<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>'),
@@ -61,6 +80,8 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'l20nCtx!locales/{{loca
                 speed: 500
             }
         },
+        // **requestSuccess**
+        // User Request completed successfully.
         requestSuccess: function(request) {
             var msg = {
                 text: this.growlTemplate({
@@ -71,6 +92,8 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'l20nCtx!locales/{{loca
             };
             noty(_.extend({}, this.commonNotyNotification, msg));
         },
+        // **requestError**
+        // User Request failed.
         requestError: function(request) {
             var msg = {
                 text: this.growlTemplate({
@@ -80,28 +103,42 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'l20nCtx!locales/{{loca
             };
             noty(_.extend({}, this.commonNotyNotification, msg));
         },
+        // **notyDefaults**
+        // Default noty values for general notifications.
         notyDefaults: {
             layout: 'top',
             type: 'error'
         },
-        timeoutCount: 0,
+        // **error**
+        // Error Helper.
         error: function(msg) {
             noty(msg);
         },
+        // **wwarning**
+        // Warning helper.
         warning: function(msg) {
             msg.type = 'warning';
             noty(msg);
         },
+        // **timeout**
+        // Dashboard timeout helper. If a request to the Calamari API timeouts,
+        // this message is shown. These are usually transient errors.
         timeout: function(msg) {
             msg.text = l10n.getSync('dashboardUpdateTimeout');
             this.timeoutCount++;
             this.error(msg);
             console.log('timeout count ' + this.timeoutCount);
         },
+        // **clusterAPITimeout**
+        // Cluster API backend may not be responding message.
         clusterAPITimeout: function(msg) {
             msg.text = l10n.getSync('clusterNotResponding');
             this.warning(msg);
         },
+        // **sessionExpired**
+        // Special message indicating user session has expired.
+        // User is required to log back in. Redirect user back to
+        // login app.
         sessionExpired: function(msg) {
             msg = _.extend(msg, {
                 text: l10n.getSync('sessionTimeout'),
@@ -117,14 +154,21 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'l20nCtx!locales/{{loca
             });
             this.warning(msg);
         },
+        // **serverError**
+        // Generic Server Error handler.
         serverError: function(msg, xhr) {
             msg.text = l10n.getSync('serverErrorMessage', xhr);
             this.error(msg);
         },
+        // **unexpectedError**
+        // Unexpected error handler.
         unexpectedError: function(msg, xhr) {
             msg.text = l10n.getSync('unexpectedError', xhr);
             this.error(msg);
         },
+        // **serverUnreachable**
+        // If the server API stops responding completely, say due to the server being
+        // down, block all further interaction with user and have them try re-loading.
         serverUnreachable: function(msg, xhr) {
             msg = _.extend(msg, {
                 force: true,
@@ -133,8 +177,11 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'l20nCtx!locales/{{loca
                 closeWith: []
             });
             this.error(msg);
+            // Disable the error and warning helpers.
             this.error = this.warning = _.identity;
         },
+        // **parserError**
+        // A JSON parsing error was encountered while trying to parse config.json.
         parserError: function(msg, xhr) {
             msg = _.extend(msg, {
                 text: l10n.getSync('JSONParserError', xhr),
@@ -142,12 +189,9 @@ define(['jquery', 'underscore', 'backbone', 'templates', 'l20nCtx!locales/{{loca
             });
             this.error(msg);
         },
-        configError: function(str) {
-            var msg = _.extend({}, this.notyDefaults, {
-                text: str
-            });
-            this.error(msg);
-        },
+        // **neterrorHandler**
+        // Dispatch error messages for the different types of
+        // network error that can occur.
         neterrorHandler: function(source, xhr) {
             var errorType = xhr.statusText;
             if (!errorType) {
