83d8c389502b153908631caf10a7a673d7d79020
Yan-Fa Li <yanfali@gmail.com>
2014-05-29 16:37:57 -0700

    Updated key, osd and server docs
    - improved markup
    
    Signed-off-by: Yan-Fa Li <yanfali@gmail.com>

--- manage/app/scripts/services/key-svc.js
+++ manage/app/scripts/services/key-svc.js
@@ -1,7 +1,7 @@
 /*global define*/
 define(['lodash'], function(_) {
     'use strict';
-    // Wraps the /api/v2/key Calamari API with a Service.
+    // Wraps the **/api/v2/key** Calamari API anchor with an AngularJS Service.
     var KeyService = function(Restangular, ErrorService) {
         // Basic Restangular instance.
         var restangular = Restangular.withConfig(function(RestangularConfigurer) {
@@ -11,21 +11,21 @@ define(['lodash'], function(_) {
         var restangularFull = Restangular.withConfig(function(RestangularConfigurer) {
             RestangularConfigurer.setBaseUrl('/api/v2').setFullResponse(true).setErrorInterceptor(ErrorService.errorInterceptor);
         });
-        // Constructor
+        // **Constructor**
         var Service = function() {
             this.restangular = restangular;
             this.restangularFull = restangularFull;
         };
         Service.prototype = _.extend(Service.prototype, {
             // **getList**
-            // Return all Minion Keys this calamari server has registered.
+            // **@returns** A promise with all Minion Keys this calamari server has registered.
             getList: function() {
                 return this.restangular.all('key').getList().then(function(keys) {
                     return keys;
                 });
             },
             // **get**
-            // Return a specific Minion Key.
+            // **@returns** a promise with a specific Minions Key data.
             get: function(id) {
                 return this.restangular.one('key', id).get().then(function(key) {
                     return key[0];
@@ -34,6 +34,9 @@ define(['lodash'], function(_) {
             // **accept**
             // Request that the array of ids be set to accepted state.
             // Used to accept new salt minions into Calamari.
+            // **@param** ids - array of pending ids we wish to accept as minions
+            // **@returns** a promise with the result of the request normally a status
+            // code of 202.
             accept: function(ids) {
                 var accepted = _.map(ids, function(id) {
                     return {
--- manage/app/scripts/services/osd-svc.js
+++ manage/app/scripts/services/osd-svc.js
@@ -1,7 +1,7 @@
 /*global define*/
 define(['lodash'], function(_) {
     'use strict';
-    // Wraps the **/api/v2/cluster/<fsid>/osd** service anchor point.
+    // Wraps the **/api/v2/cluster/&lt;fsid&gt;/osd** service anchor point.
     //
     var OSDService = function(ClusterService) {
         // **Constructor**
@@ -10,7 +10,7 @@ define(['lodash'], function(_) {
         };
         Service.prototype = _.extend(Service.prototype, {
             // **getList**
-            // @returns promise
+            // **@returns** promise with the entire list of OSDs known about.
             getList: function() {
                 return this.restangular.cluster().getList('osd').then(function(osds) {
                     return osds;
@@ -18,8 +18,8 @@ define(['lodash'], function(_) {
             },
             // **getSet**
             // Return a specific subset of OSDs specified by OSD ID.
-            // @param *ids* - array of ids as numbers.
-            // @returns promise
+            // **@param** *ids* - array of ids as numbers.
+            // **@returns** a promise with the set of specified OSDs metadata.
             getSet: function(ids) {
                 var idargs = _.reduce(ids, function(result, id) {
                     result.push(id);
@@ -32,8 +32,8 @@ define(['lodash'], function(_) {
                 });
             },
             // **get**
-            // @param *id* - id as number of OSD.
-            // @returns promise
+            // **@param** *id* - id as number of OSD.
+            // **@returns** promise with the specified OSD metadata
             get: function(id) {
                 id = _.isString(id) ? parseInt(id, 10) : id;
                 return this.restangular.cluster().one('osd', id).get().then(function(osd) {
@@ -41,59 +41,61 @@ define(['lodash'], function(_) {
                 });
             },
             // **patch**
-            // @param *id* - id of OSD you wish to patch.
+            // **@param** *id* - id of OSD you wish to patch.
             //             ID must be parseable by parseInt.
-            // @param *update* - object containing key value pairs you want to update.
+            // **@param** *update* - object containing key value pairs you want to update.
+            // **@returns** a promise with result of operation, a status code.
+            // 
             patch: function(id, update) {
                 id = _.isString(id) ? parseInt(id, 10) : id;
                 return this.restangular.clusterFull().one('osd', id).patch(update);
             },
             // **down**
-            // @param *id* - id of the OSD you wish to set to down.
+            // **@param** *id* - id of the OSD you wish to set to down.
             //        There is no way to set the OSD back to **up**.
             //        Ceph automatically promotes healthy down OSDs 
             //        back to **up** unless a noup flag is set on the
             //        cluster.
-            // @return promise
+            // **@returns** promise with result of operation, **@see** patch
             down: function(id) {
                 return this.patch(id, {
                     up: false
                 });
             },
             // **out**
-            // @param *id* - id of the OSD you wish to set to out.
-            // @return promise
+            // **@param** *id* - id of the OSD you wish to set to out.
+            // **@returns** promise with result of operation, **@see** patch
             out: function(id) {
                 return this.patch(id, {
                     'in': false
                 });
             },
             // **in**
-            // @param *id* - id of the OSD you wish to set to in.
-            // @return promise
+            // **@param** *id* - id of the OSD you wish to set to in.
+            // **@returns** promise with result of operation, **@see** patch
             in : function(id) {
                 return this.patch(id, {
                     'in': true
                 });
             },
             // **scrub**
-            // @param *id* - id of the OSD you wish send a scrub command to.
-            // @return promise
+            // **@param** *id* - id of the OSD you wish send a scrub command to.
+            // **@returns** promise with result of operation, **@see** patch
             scrub: function(id) {
                 id = _.isString(id) ? parseInt(id, 10) : id;
                 return this.restangular.clusterFull().one('osd', id).all('command').all('scrub').post({});
             },
             /* jshint camelcase: false */
             // **deep_scrub**
-            // @param *id* - id of the OSD you wish send a deep scrub command to.
-            // @return promise
+            // **@param** *id* - id of the OSD you wish send a deep scrub command to.
+            // **@returns** promise with result of operation, **@see** patch
             deep_scrub: function(id) {
                 id = _.isString(id) ? parseInt(id, 10) : id;
                 return this.restangular.clusterFull().one('osd', id).all('command').all('deep_scrub').post({});
             },
             // **repair**
-            // @param *id* - id of the OSD you wish send a repair command to.
-            // @return promise
+            // **@param** *id* - id of the OSD you wish send a repair command to.
+            // **@returns** promise with result of operation, **@see** patch
             repair: function(id) {
                 id = _.isString(id) ? parseInt(id, 10) : id;
                 return this.restangular.clusterFull().one('osd', id).all('command').all('repair').post({});
