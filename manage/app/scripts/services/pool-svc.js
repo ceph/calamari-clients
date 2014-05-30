af9f9c4476691c46c59cfeea56c1df3cc8e3a97c
Yan-Fa Li <yanfali@gmail.com>
2014-05-29 18:29:51 -0700

    Add docco for pool-svc and request-svc
    Signed-off-by: Yan-Fa Li <yanfali@gmail.com>

--- manage/app/scripts/services/pool-svc.js
+++ manage/app/scripts/services/pool-svc.js
@@ -1,33 +1,59 @@
 /*global define*/
 define(['lodash'], function(_) {
     'use strict';
+    // Wraps the **api/v2/cluster/&lt;fsid&gt;/pool** API end-point.
+    // 
     var PoolService = function(ClusterService) {
+
+        // **Constructor**
         var Service = function() {
             this.restangular = ClusterService;
         };
+
         Service.prototype = _.extend(Service.prototype, {
+            // **getList**
+            // **@returns** a promise which has the list of all the
+            // pools being managed by this Cluster.
             getList: function() {
                 return this.restangular.cluster().all('pool').getList().then(function(pools) {
                     return pools;
                 });
             },
+            // **get**
+            // **@param** id - id of the pool you wish to retrieve
+            // **@returns** a promise with the meta data associated with this pool.
             get: function(id) {
                 id = _.isString(id) ? parseInt(id, 10) : id;
                 return this.restangular.cluster().one('pool', id).get().then(function(pool) {
                     return pool;
                 });
             },
+            // **remove**
+            // **@param** id - id of pool you wish to remove.
+            // This is a **destructive** operation and will delete
+            // any data on this pool.
+            // **@returns** a promise with the request id for the operation.
             remove: function(id) {
                 id = _.isString(id) ? parseInt(id, 10) : id;
                 return this.restangular.clusterFull().one('pool', id).remove();
             },
+            // **patch**
+            // **@param** id - id of pool you are trying to patch.
+            // **@param** update - object with key value pairs you are changing.
+            // **@returns** a promise with the request id for the operation.
             patch: function(id, update) {
                 id = _.isString(id) ? parseInt(id, 10) : id;
                 return this.restangular.clusterFull().one('pool', id).patch(update);
             },
+            // **create**
+            // **@param** pool - object with key value pairs used to configure a new pool.
+            // **@returns** a promise which returns a request id to track the task.
             create: function(pool) {
                 return this.restangular.clusterFull().all('pool').post(pool);
             },
+            // **defaults**
+            // **@returns** the cluster pool default values.
+            // We're mostly interesting in size and crush_ruleset at this time.
             defaults: function() {
                 return this.restangular.cluster().one('pool').get({
                     defaults: ''
--- manage/app/scripts/services/request-svc.js
+++ manage/app/scripts/services/request-svc.js
@@ -1,12 +1,26 @@
 /*global define*/
 define(['lodash'], function(_) {
     'use strict';
+    // Default number of results in a request.
     var pageSize = 32;
+    // Wraps the **/api/v2/cluster/&lt;fsid&gt;/request** API end-point.
+    // This API is our interface in the User Request queue managed by
+    // Calamari. When a request returns a 202 success code and a
+    // request id.
+    //
+    // This request id is cached locally using indexdb and then we
+    // periodically check the submitted queue to see if it still
+    // running.
+    //
     var RequestService = function(ClusterService) {
+
+        // **constructor**
         var Service = function() {
             this.restangular = ClusterService;
         };
         Service.prototype = _.extend(Service.prototype, {
+            // **getList**
+            // **@returns** a promise with most recent *pageSize* requests.
             getList: function() {
                 /* jshint camelcase: false */
                 return this.restangular.cluster().customGETLIST('request', {
@@ -15,11 +29,15 @@ define(['lodash'], function(_) {
                     return requests;
                 });
             },
+            // **get**
+            // **@returns** a promise with a single request by it's ID.
             get: function(id) {
                 return this.restangular.clusterFull().one('request', id).get().then(function(resp) {
                     return resp.data;
                 });
             },
+            // **getComplete**
+            // **@returns** a promise with most recent *pageSize* completed requests.
             getComplete: function() {
                 /* jshint camelcase: false */
                 return this.restangular.cluster().customGETLIST('request', {
@@ -27,6 +45,8 @@ define(['lodash'], function(_) {
                     page_size: pageSize
                 });
             },
+            // **getComplete**
+            // **@returns** a promise with most recent *pageSize* submitted requests.
             getSubmitted: function() {
                 /* jshint camelcase: false */
                 return this.restangular.cluster().customGETLIST('request', {
