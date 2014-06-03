baab7916bc89edbb8d7723a4c172e4577338575f
Yan-Fa Li <yanfali@gmail.com>
2014-06-03 16:52:23 -0700

    Docco first.js
    first.js bootstraps a brand new Calamari Install. Add
    some annotations about what the controller is doing.
    
    Signed-off-by: Yan-Fa Li <yanfali@gmail.com>

--- manage/app/scripts/controllers/first.js
+++ manage/app/scripts/controllers/first.js
@@ -4,19 +4,35 @@
     define(['lodash', 'helpers/modal-helpers'], function(_) {
 
         var clusterPollIntervalMs = 1000;
+        // **FirstTimeController** should only be invoked on a fresh Calamari install where there are
+        // no currently accepted cluster hosts. Calamari root.js detects this condition by requesting a list of
+        // the current clusters and if there are none, redirects to this controller.
+        //
+        // We handle the initial bootstrapping of Calamari by offering a visual way for the end-user
+        // to accept all the registered hosts. This makes the assumption that they are all members of
+        // the same cluster. Something which may not necessarily be true if this is a multi-cluster
+        // installation.
+        //
         var FirstTimeController = function($q, $log, $timeout, $location, $scope, KeyService, ClusterService, $modal) {
             var promises = [KeyService.getList()];
+            // Start the view out with adds disabled via ng-disabled
             $scope.addDisabled = true;
             $scope.debug = false;
             $q.all(promises).then(function(results) {
 
+                // **up** is used to control whether the view is rendered yet via ng-show.
                 $scope.up = true;
                 if (ClusterService.clusterId !== null) {
+                    // The user has navigated to this URL erroneously, send them back to
+                    // the default cluster page.
                     $location.path('/');
                     return;
                 }
 
+                // Process the returned list of server minion key states.
                 (function(keys) {
+                    // Bucket the keys in **pre**, **accepted** and **blocked** sets.
+                    // We really only care about accepted ones.
                     $scope.hosts = _.reduce(keys, function(result, key) {
                         if (key.status === 'pre') {
                             result.pre.push(key);
@@ -31,13 +47,22 @@
                         pre: [],
                         blocked: []
                     });
+                    // Re-enable add button via ng-disabled
                     $scope.addDisabled = false;
+
+                    // Install the click event handler.
                     $scope.acceptAll = function() {
+                        // get the ids of all pre-accepted hosts
                         var ids = _.map($scope.hosts.pre, function(key) {
                             return key.id;
                         });
                         $log.debug(ids);
+                        // Disable add button via ng-disabled because we are now done
+                        // unless there is an error.
                         $scope.addDisabled = true;
+
+                        // Create a modal we will show while we are waiting for Calamari to accept
+                        // the servers.
                         var modal = $modal({
                             'title': '<i class="text-success fa fa-check-circle fa-lg"></i> Accept Request Sent',
                             'template': 'views/new-install-modal.html',
@@ -45,7 +70,11 @@
                             'backdrop': 'static',
                             'html': true
                         });
+                        // Disable closing of the modal while we are in this state.
                         modal.$scope.closeDisabled = true;
+
+                        // Decorate the $hide method from angular-strap so we can
+                        // add some behavior to it.
                         modal.$scope.$hide = _.wrap(modal.$scope.$hide, function($hide) {
                             $hide();
                             ClusterService.initialize().then(function() {
@@ -53,24 +82,33 @@
                             });
                         });
 
+                        // Helper to poll Calamari until the cluster API responds
+                        // with an actual cluster data structure.
                         function checkClusterUp() {
                             ClusterService.getList().then(function(clusters) {
                                 if (clusters.length) {
+                                    // A Cluster is now available. Release the modal
+                                    // lockdown and tell the user.
                                     modal.$scope.closeDisabled = false;
                                     modal.$scope.content = 'Cluster Initialized.';
                                     return;
                                 }
+                                // Schedule poll again - Calamari is still working.
                                 $timeout(checkClusterUp, clusterPollIntervalMs);
                             });
                         }
+
+                        // Send the Accept command to Calamari and start polling.
                         KeyService.accept(ids).then(function(resp) {
                             $log.debug(resp);
                             if (resp.status === 204) {
+                                // Start polling loop
                                 $timeout(checkClusterUp, clusterPollIntervalMs);
                             }
                             $scope.addDisabled = false;
 
                         }, function(resp) {
+                            // Uh-oh, an error occurred. Tell the user.
                             modal.$scope.content = '<i class="text-danger fa fa-exclamation-circle fa-lg"></i> Error ' + resp.status + '. Please try reloading the page and logging in again.</p><h4>Raw Response</h4><p><pre>' + resp.data + '</pre></p>';
                             $scope.addDisabled = false;
                         });
